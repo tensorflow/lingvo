5abe8d0dc49343b59c5e6d8f653ea217e84494c5
jonathanasdf <jonathanasdf@google.com>
2018-11-02 21:06:14 -0700

    Fix uninitialized empty arrays in recurrent.py.
    PiperOrigin-RevId: 219903505

--- lingvo/core/recurrent.py
+++ lingvo/core/recurrent.py
@@ -233,7 +233,8 @@ def _EmptyWithFixShape(shape, nmap):
     same dtype as nmap.key, but with the fixed shape.
   """
 
-  return nmap.Transform(lambda x: inplace_ops.empty(shape, dtype=x.dtype))
+  return nmap.Transform(
+      lambda x: inplace_ops.empty(shape, dtype=x.dtype, init=True))
 
 
 def _EmptyLike(nmap):
