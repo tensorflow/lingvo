2f13e61e01a3655dea495378e75b34a3386eaf4d
drpngx <drpngx@users.noreply.github.com>
2019-03-05 17:47:14 -0800

    remove extra space

--- lingvo/core/beam_search_helper.py
+++ lingvo/core/beam_search_helper.py
@@ -91,7 +91,8 @@ class BeamSearchHelper(base_layer.BaseLayer):
           log_probs: The initial log probs for each of the tokens in
               the target vocab, of shape [tgt_batch * num_hyps_per_beam, 
               vocab_size].
-          atten_probs: The initial attention probs, of shape [
+          
+          s: The initial attention probs, of shape [
               tgt_batch * num_hyps_per_beam, src_len].
           states: a `.NestedMap` of tensors representing states that the client
               would like to keep track of for each hyp.
@@ -118,10 +119,10 @@ class BeamSearchHelper(base_layer.BaseLayer):
               the 'atten_probs' and 'log_probs' tensors at the minimal.
               Optionally it may contain 'is_last_chunk' if it is decoding a
               neural transducer model.
-           atten_probs: The updated attention probs, of shape [tgt_batch, src_len].
-           log_probs: Log prob for each of the tokens in the target vocab. This
-               is of shape [tgt_batch, vocab_size].
-           is_last_chunk: Whether or not each of the hyp is at the end of a
+          atten_probs: The updated attention probs, of shape [tgt_batch, src_len].
+          log_probs: Log prob for each of the tokens in the target vocab. This
+              is of shape [tgt_batch, vocab_size].
+          is_last_chunk: Whether or not each of the hyp is at the end of a
               chunk. If non-empty, it is of shape [tgt_batch, 1]
           out_states: A `.NestedMap`. The updated states. This 'out_states'
               should be of the exact same structure as 'in_states'
