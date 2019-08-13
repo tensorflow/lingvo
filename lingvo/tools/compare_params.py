3b9ea092ab46fc4eeb0bc67dc191dd661c315c07
Vijay Vasudevan <vrv@google.com>
2019-08-13 15:29:28 -0700

    Add compare_params.py helper and test.
    Used to fetch params from registered config or from a filepath,
    with a helper to locally produce a helpful diff of two configs
    for inspection.
    
    One can also just write the ToText() outputs to two files and diff
    them using your favorite diff tool.
    
    PiperOrigin-RevId: 263228906

---
+++ lingvo/tools/compare_params.py
@@ -0,0 +1,108 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Library for comparing two models / hyperparams."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo import model_registry
+
+
+def _hyperparams_text_to_dict(cfg_text):
+  """Converts hyperparams config text to a dictionary of key-value pairs."""
+  txt_list = cfg_text.split("\n")
+  pair_list = []
+  for v in txt_list:
+    if not v:
+      continue
+    vals = v.split(" : ")
+    if len(vals) != 2:
+      raise ValueError(v)
+    pair_list.append(vals)
+  return dict(pair_list)
+
+
+def hyperparams_text_diff(cfg1_text, cfg2_text):
+  """Computes the differences between two hyperparams.Params texts.
+
+  Args:
+    cfg1_text: A hyperparams.Params().ToText() of the first model config.
+    cfg2_text: A hyperparams.Params().ToText() of the second model config.
+
+  Returns:
+    cfg1_not_cfg2: A list of keys in cfg1 but not cfg2.
+
+    cfg2_not_cfg1: A list of keys in cfg2 but not cfg1.
+
+    cfg1_and_cfg2_diff: A dictionary of common keys whose
+    config values differ: each value is a tuple of the config
+    values from cfg1 and cfg2 respectively.
+  """
+  cfg1_dict = _hyperparams_text_to_dict(cfg1_text)
+  cfg2_dict = _hyperparams_text_to_dict(cfg2_text)
+  cfg1_keys = set(cfg1_dict.keys())
+  cfg2_keys = set(cfg2_dict.keys())
+
+  cfg1_not_cfg2 = sorted(list(cfg1_keys - cfg2_keys))
+  cfg2_not_cfg1 = sorted(list(cfg2_keys - cfg1_keys))
+
+  cfg1_and_cfg2_diff = {}
+  for k_intersection in cfg1_keys & cfg2_keys:
+    c1v = cfg1_dict[k_intersection]
+    c2v = cfg2_dict[k_intersection]
+    if c1v != c2v:
+      cfg1_and_cfg2_diff[k_intersection] = (c1v, c2v)
+
+  return cfg1_not_cfg2, cfg2_not_cfg1, cfg1_and_cfg2_diff
+
+
+def print_hyperparams_text_diff(path1, path2, cfg1_not_cfg2, cfg2_not_cfg1,
+                                cfg1_and_cfg2_diff):
+  """Prints the differences of the output of hyperparams_text_diff.
+
+  Args:
+    path1: Name of registered model or path to model 1.
+    path2: Name of registered model or path to model 2.
+    cfg1_not_cfg2: A list of keys in cfg1 but not cfg2.
+    cfg2_not_cfg1: A list of keys in cfg2 but not cfg1.
+    cfg1_and_cfg2_diff: A dictionary of common keys whose config values differ;
+      each value is a tuple of the config values from cfg1 and cfg2
+      respectively.
+  """
+  if cfg1_not_cfg2:
+    print("\n\nKeys in %s but not %s: \n%s\n\n" %
+          (path1, path2, "\n".join(cfg1_not_cfg2)))
+  if cfg2_not_cfg1:
+    print("\n\nKeys in %s but not %s: \n%s\n\n" %
+          (path2, path1, "\n".join(cfg2_not_cfg1)))
+
+  if cfg1_and_cfg2_diff:
+    print("\n\nKeys with differences and their values: \n\n")
+    for k, v in sorted(cfg1_and_cfg2_diff.items()):
+      v1, v2 = v
+      print("%s: [%s] vs. [%s]" % (k, v1, v2))
+    print("\n\n")
+
+
+def get_model_params_as_text(model_path):
+  try:
+    cfg = model_registry.GetParams(model_path, "Train")
+    return cfg.ToText()
+  except LookupError:
+    # Try reading as file.
+    return tf.io.gfile.GFile(model_path).read()
---
+++ lingvo/tools/compare_params_test.py
@@ -0,0 +1,49 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for compare_params."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo.core import hyperparams
+from lingvo.core import test_utils
+from lingvo.tools import compare_params
+
+
+class CompareParamsTest(test_utils.TestCase):
+
+  def testBasic(self):
+    h1 = hyperparams.Params()
+    h1.Define('a', 1, '')
+    h1.Define('b', 2, '')
+
+    h2 = hyperparams.Params()
+    h2.Define('a', 3, '')  # different value.
+    h2.Define('c', 2, '')
+
+    d1, d2, d3 = compare_params.hyperparams_text_diff(h1.ToText(), h2.ToText())
+    self.assertEqual(d1, ['b'])
+    self.assertEqual(d2, ['c'])
+    self.assertEqual(d3, {'a': ('1', '3')})
+
+    # Exercise print function
+    compare_params.print_hyperparams_text_diff('h1', 'h2', d1, d2, d3)
+
+
+if __name__ == '__main__':
+  tf.test.main()
