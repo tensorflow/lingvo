57fa285763ccf2361f9a81fc33fcd0d154406404
Lingvo Maintenance <lingvo-bot@google.com>
2019-09-11 17:32:03 -0700

    Adds RnnStep and RnnStackStep.
    RnnStep wraps an RNNCell in the step interface. RnnStackStep represents a
    stack of RNN layers, including support for context vectors and residual
    connections.
    
    PiperOrigin-RevId: 268579164

---
+++ lingvo/core/steps/rnn_steps.py
@@ -0,0 +1,218 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Step APIs for RNN layers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo.core import base_layer
+from lingvo.core import hyperparams
+from lingvo.core import py_utils
+from lingvo.core import rnn_cell
+from lingvo.core import step
+
+
+class RnnStep(step.Step):
+  """A step containing an RNNCell."""
+
+  @classmethod
+  def Params(cls):
+    p = super(RnnStep, cls).Params()
+    p.Define('cell', rnn_cell.LSTMCellSimple.Params(),
+             'Params for the RNN cell.')
+    return p
+
+  @base_layer.initializer
+  def __init__(self, params):
+    super(RnnStep, self).__init__(params)
+    p = params
+    with tf.variable_scope(p.name):
+      self.CreateChild('cell', p.cell)
+
+  def PrepareExternalInputs(self, theta, external_inputs):
+    """Does not modify the external_inputs parameter.
+
+    This parameter, if provided, is assumed to be a vector that should be
+    concatenated with the other vectors in step_inputs.inputs.
+
+    Args:
+      theta: unused.
+      external_inputs: Either a tensor or None.
+
+    Returns:
+      external_inputs, unmodified.
+    """
+    return external_inputs
+
+  def ZeroState(self, theta, external_inputs, batch_size):
+    """Returns the zero_state for the RNN cell.
+
+    Args:
+      theta: Variables used by the RNNCell.
+      external_inputs: unused.
+      batch_size: An int scalar representing the batch size of per-step inputs.
+
+    Returns:
+      The zero state of the RNNCell.
+    """
+    return self.cell.zero_state(theta.cell, batch_size)
+
+  def FProp(self, theta, external_inputs, step_inputs, padding, state0):
+    """Performs one inference step on the RNN cell.
+
+    If external_inputs is not None, it is added as another act input
+    to the RNNCell.
+
+    Args:
+      theta: Variables used by the RNNCell.
+      external_inputs: If not None, concatenated with step_inputs.input. A
+        tensor of shape [batch_size, external_input_dim].
+      step_inputs: A NestedMap containing an 'input' list of [batch_size, dim]
+        where the sum of dim (including external_inputs) is
+        p.cell.num_input_nodes.
+      padding: A 0/1 float tensor of shape [batch_size]; 1.0 means that this
+        batch element is empty in this step.
+      state0: A NestedMap of state, either produced by ZeroState or a previous
+        invocation of FProp.
+
+    Returns:
+      (output, state1), where output is the cell output (GetOutput(state1))
+      of shape [batch_size, p.cell.num_output_nodes], and state1 is the cell's
+      recurrent state.
+    """
+    cell_inputs = py_utils.NestedMap(act=step_inputs.inputs)
+    if external_inputs is not None:
+      cell_inputs.act.append(external_inputs)
+    cell_inputs.padding = padding
+    state1, extra = self.cell.FProp(theta.cell, state0, cell_inputs)
+    return py_utils.NestedMap(
+        output=self.cell.GetOutput(state1), extra=extra,
+        padding=padding), state1
+
+
+class RnnStackStep(object):
+  """A stack of RnnSteps.
+
+  Three types of inputs are supported:
+    step_inputs.input: This is the standard input. It is expected to change
+      on every step of the sequence, and it is fed only to the first layer.
+    step_inputs.context: This input changes for each step of the sequence, but
+      is fed to every layer.
+    external_inputs: This input is fixed at the beginning of the sequence.
+      It is fed to every layer.
+
+  Residual connections are also supported. When residual_start > 0, the output
+  of layer i (i >= residual_start) is added to the output of layer
+  i - residual_stride.
+  """
+
+  @classmethod
+  def Params(cls):
+    """Constructs Params for an RnnStackStep."""
+    p = RnnStackStep.InternalParams()
+    p.Define('name', None, 'Name of this layer.')
+    p.Define(
+        'rnn_cell_tpl', rnn_cell.LSTMCellSimple.Params(),
+        'RNNCell params template. '
+        'Can be a single param or '
+        'a list of rnn_layers params, one for each layer.')
+    p.Define(
+        'external_input_dim', 0, 'Size of the external input. '
+        'The external input is given at the start of the sequence '
+        'and is given to every layer at every step.')
+    p.Define(
+        'step_input_dim', 0, 'Size of the step input. '
+        'This input is only given to the first layer and is expected to '
+        'be different for each step.')
+    p.Define(
+        'context_input_dim', 0, 'Size of the context input. '
+        'This input is given to every layer and is expected to be '
+        'different for each step.')
+    p.Define(
+        'rnn_cell_dim', 0, 'Size of the rnn cells. '
+        'This may be overridden by parameters set in rnn_cell_tpl.')
+    p.Define(
+        'rnn_cell_hidden_dim', 0, 'internal size of the rnn cells. When '
+        'set to > 0 it enables a projection layer at the output of the '
+        'rnn cell. This may be overridden by parameters set in rnn_cell_tpl.')
+    p.Define('rnn_layers', 1, 'Number of rnn layers.')
+    p.Define(
+        'residual_start', 0,
+        'Start residual connections from this layer. For this and higher '
+        'layers, the layer output is the sum of the RNN cell output and '
+        'input; if the layer also normalizes its output, then the '
+        'normalization is done over this sum. Set to 0 to disable '
+        'residual connections.')
+    p.Define('residual_stride', 1,
+             'Number of lstm layers to skip per residual connection.')
+    return p
+
+  class InternalParams(hyperparams.InstantiableParams):
+    """A hyperparams object that converts itself into a StackStep.
+
+    This object operates like a normal InstantiatableParams, but instead
+    of using the default Instantiate method, it converts its params into
+    StackStep params and instantiates a StackStep.
+    """
+
+    def __init__(self):
+      super(RnnStackStep.InternalParams, self).__init__(step.StackStep)
+
+    def Instantiate(self):
+      sub = []
+      p = self
+
+      # Users can either provide a single rnn_cell_tpl or one per layer.
+      # If only one is provided, we replicate it for each layer.
+      rnn_cell_tpls = p.rnn_cell_tpl
+      if not isinstance(rnn_cell_tpls, list):
+        rnn_cell_tpls = [p.rnn_cell_tpl] * p.rnn_layers
+
+      # We may provide up to three tensors as input to the RnnStep:
+      # the normal input, the context input (from step_inputs.context),
+      # and the external input (from external_inputs).
+      arity = 1
+      if p.context_input_dim:
+        arity += 1
+      if p.external_input_dim:
+        arity += 1
+      extra_dim = p.context_input_dim + p.external_input_dim
+
+      # The first layer's input comes from step_inputs.input. Later layers
+      # will get their inputs from the previous layer's output.
+      input_nodes = p.step_input_dim
+      for i in range(p.rnn_layers):
+        step_i = RnnStep.Params()
+        step_i.name = 'rnn_%d' % (i)
+        step_i.cell = rnn_cell_tpls[i].Copy()
+        step_i.cell.num_input_nodes = input_nodes + extra_dim
+        step_i.cell.inputs_arity = arity
+        # The dimensions of each cell may be specified in the cell template
+        # but most users will specify them in the stack params.
+        if step_i.cell.num_output_nodes == 0:
+          step_i.cell.num_output_nodes = p.rnn_cell_dim
+        if step_i.cell.num_hidden_nodes == 0:
+          step_i.cell.num_hidden_nodes = p.rnn_cell_hidden_dim
+        input_nodes = step_i.cell.num_output_nodes
+        sub.append(step_i)
+
+      stack_params = step.StackStep.Params()
+      stack_params.name = p.name
+      stack_params.sub = sub
+      stack_params.residual_start = p.residual_start
+      stack_params.residual_stride = p.residual_stride
+      return stack_params.Instantiate()
---
+++ lingvo/core/steps/rnn_steps_test.py
@@ -0,0 +1,200 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for lingvo.core.steps.rnn_steps."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo.core import py_utils
+from lingvo.core import recurrent
+from lingvo.core import test_utils
+from lingvo.core.steps import rnn_steps
+
+
+class RnnStepsTest(test_utils.TestCase):
+
+  def testRnnStep(self):
+    with self.session(use_gpu=False) as sess:
+      p = rnn_steps.RnnStep.Params()
+      p.name = 'rnn_step'
+      p.cell.name = 'rnn_step_cell'
+      p.cell.output_nonlinearity = True
+      p.cell.params_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.cell.bias_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.cell.vn.global_vn = False
+      p.cell.vn.per_step_vn = False
+      p.cell.num_input_nodes = 2
+      p.cell.num_output_nodes = 2
+      p.cell.inputs_arity = 2
+
+      rnn_step = p.Instantiate()
+      external = tf.constant([[3]], tf.float32)
+      packed = rnn_step.PrepareExternalInputs(rnn_step.theta, external)
+
+      state0 = rnn_step.ZeroState(rnn_step.theta, packed, 1)
+      _, state1 = rnn_step.FProp(
+          rnn_step.theta, packed,
+          py_utils.NestedMap(inputs=[tf.constant([[4]], tf.float32)]),
+          tf.constant([0.0], dtype=tf.float32), state0)
+      out2, state2 = rnn_step.FProp(
+          rnn_step.theta, packed,
+          py_utils.NestedMap(inputs=[tf.constant([[4]], tf.float32)]),
+          tf.constant([0.0], dtype=tf.float32), state1)
+
+      tf.global_variables_initializer().run()
+      out2, state2 = sess.run([out2, state2])
+
+      self.assertAllClose(state2.m, [[-0.32659757, 0.87739915]])
+      self.assertAllClose(state2.c, [[-1.9628618, 1.4194499]])
+      self.assertAllClose(out2.output, [[-0.32659757, 0.87739915]])
+      self.assertAllClose(out2.padding, [0.0])
+
+  def testRnnStepRecurrent(self):
+    with self.session(use_gpu=False) as sess:
+      p = rnn_steps.RnnStep.Params()
+      p.name = 'rnn_step'
+      p.cell.name = 'rnn_step_cell'
+      p.cell.output_nonlinearity = True
+      p.cell.params_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.cell.bias_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.cell.vn.global_vn = False
+      p.cell.vn.per_step_vn = False
+      p.cell.num_input_nodes = 2
+      p.cell.num_output_nodes = 2
+      p.cell.inputs_arity = 2
+
+      rnn_step = p.Instantiate()
+      external = tf.constant([[3]], tf.float32)
+      packed = rnn_step.PrepareExternalInputs(rnn_step.theta, external)
+      padding = tf.constant([0.0], dtype=tf.float32)
+
+      def cell_fn(theta, state0, inputs):
+        output, state1 = rnn_step.FProp(theta, state0.packed, inputs,
+                                        state0.padding, state0.state)
+        return py_utils.NestedMap(
+            output=output,
+            state=state1,
+            packed=state0.packed,
+            padding=state0.padding), py_utils.NestedMap()
+
+      _, state2 = recurrent.Recurrent(
+          theta=rnn_step.theta,
+          state0=py_utils.NestedMap(
+              state=rnn_step.ZeroState(rnn_step.theta, packed, 1),
+              output=py_utils.NestedMap(
+                  output=tf.zeros([1, 2], tf.float32),
+                  extra=py_utils.NestedMap(),
+                  padding=padding),
+              padding=padding,
+              packed=packed),
+          cell_fn=cell_fn,
+          inputs=py_utils.NestedMap(
+              inputs=[tf.constant([[[4]], [[4]]], tf.float32)]))
+
+      tf.global_variables_initializer().run()
+      state2 = sess.run(state2)
+      self.assertAllClose(state2.state.m, [[-0.32659757, 0.87739915]])
+      self.assertAllClose(state2.state.c, [[-1.9628618, 1.4194499]])
+      self.assertAllClose(state2.output.output, [[-0.32659757, 0.87739915]])
+
+  def testRnnStackStep(self):
+    with self.session(use_gpu=False) as sess:
+      p = rnn_steps.RnnStackStep.Params()
+      p.name = 'rnn_stack_step'
+      p.rnn_cell_tpl.params_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.rnn_cell_tpl.bias_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.rnn_cell_tpl.vn.global_vn = False
+      p.rnn_cell_tpl.vn.per_step_vn = False
+      p.external_input_dim = 2
+      p.step_input_dim = 1
+      p.context_input_dim = 1
+      p.rnn_cell_dim = 3
+      p.rnn_cell_hidden_dim = 3
+      p.rnn_layers = 3
+      p.residual_start = 0
+      rnn_stack = p.Instantiate()
+
+      external = tf.constant([[1, 2]], tf.float32)
+      packed = rnn_stack.PrepareExternalInputs(rnn_stack.theta, external)
+      state0 = rnn_stack.ZeroState(rnn_stack.theta, packed, 1)
+      output1, state1 = rnn_stack.FProp(
+          rnn_stack.theta, packed,
+          py_utils.NestedMap(
+              inputs=[tf.constant([[4]], tf.float32)],
+              context=tf.constant([[5]], tf.float32)),
+          tf.constant([0.0], dtype=tf.float32), state0)
+
+      tf.global_variables_initializer().run()
+      output1, state1 = sess.run([output1, state1])
+
+      # Because there are residual connections, we expect the output to
+      # be equal to sum(rnn_state.m for rnn_state in state1.sub).
+      self.assertAllClose(output1.output, [[3.5457525, 4.576743, 4.2290816]])
+      self.assertAllClose(
+          state1, {
+              'sub': [{
+                  'm': [[-0.4587491, 0.56409806, 0.23025148]],
+                  'c': [[5.6926787e-01, 6.3084178e-02, 4.1969700e-04]]
+              }, {
+                  'm': [[0.00226134, 0.00633766, -0.00058896]],
+                  'c': [[0.00088014, 0.07547389, 0.00625527]]
+              }, {
+                  'm': [[0.00224019, 0.00630755, -0.00058099]],
+                  'c': [[0.00088081, 0.07505893, 0.00621293]]
+              }]
+          })
+
+  def testRnnStackStepNoContext(self):
+    with self.session(use_gpu=False) as sess:
+      p = rnn_steps.RnnStackStep.Params()
+      p.name = 'rnn_stack_step'
+      p.rnn_cell_tpl.params_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.rnn_cell_tpl.bias_init = py_utils.WeightInit.Uniform(1.24, 429891685)
+      p.rnn_cell_tpl.vn.global_vn = False
+      p.rnn_cell_tpl.vn.per_step_vn = False
+      p.step_input_dim = 1
+      p.rnn_cell_dim = 3
+      p.rnn_cell_hidden_dim = 3
+      p.rnn_layers = 2
+      p.residual_start = 0
+      rnn_stack = p.Instantiate()
+
+      packed = rnn_stack.PrepareExternalInputs(rnn_stack.theta, None)
+      state0 = rnn_stack.ZeroState(rnn_stack.theta, packed, 1)
+      output1, state1 = rnn_stack.FProp(
+          rnn_stack.theta, packed,
+          py_utils.NestedMap(inputs=[tf.constant([[4]], tf.float32)]),
+          tf.constant([0.0], dtype=tf.float32), state0)
+
+      tf.global_variables_initializer().run()
+      output1, state1 = sess.run([output1, state1])
+
+      self.assertAllClose(output1.output, [[5.900284, 3.0231729, 3.0207822]])
+      self.assertAllClose(
+          state1, {
+              'sub': [{
+                  'm': [[1.1416901, -0.32166323, -0.5909376]],
+                  'c': [[-0.98086286, 0.9052862, 0.10041453]]
+              }, {
+                  'm': [[0.7585938, -0.655164, -0.3882802]],
+                  'c': [[-8.3011830e-01, 1.8685710e-01, 1.0723456e-04]]
+              }]
+          })
+
+
+if __name__ == '__main__':
+  tf.test.main()
