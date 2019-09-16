411768a7efacf4cadcd0a15902d9fa036ba9b6f7
Lingvo Maintenance <lingvo-bot@google.com>
2019-09-15 18:30:04 -0700

    Adds EmbeddingStep.
    EmbeddingStep is a simple Step wrapper around EmbeddingLayer and its
    subclasses. This allows embedding lookups to be added to StackStep or
    GraphStep.
    
    PiperOrigin-RevId: 269227994

---
+++ lingvo/core/steps/embedding_steps.py
@@ -0,0 +1,98 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Step classes for embedding tables."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo.core import base_layer
+from lingvo.core import layers
+from lingvo.core import py_utils
+from lingvo.core import step
+
+
+class EmbeddingStep(step.Step):
+  """A simple wrapper around EmbeddingLayer and its subclasses.
+
+  This class can be used to insert an embedding lookup at the input side
+  of a GraphStep or StackStep.
+  """
+
+  @classmethod
+  def Params(cls):
+    p = super(EmbeddingStep, cls).Params()
+    p.name = 'emb_step'
+    p.Define('emb', layers.EmbeddingLayer.Params(), 'Embedding layer params.')
+    return p
+
+  @base_layer.initializer
+  def __init__(self, params):
+    super(EmbeddingStep, self).__init__(params)
+    p = params
+    with tf.variable_scope(p.name):
+      self.CreateChild('emb', p.emb)
+
+  def PrepareExternalInputs(self, theta, external_inputs):
+    """External inputs are unused for EmbeddedStep.
+
+    Args:
+      theta: unused.
+      external_inputs: unused.
+
+    Returns:
+      an empty NestedMap.
+    """
+    return py_utils.NestedMap()
+
+  def ZeroState(self, theta, external_inputs, batch_size):
+    """EmbeddingStep has no state.
+
+    Args:
+      theta: unused.
+      external_inputs: unused.
+      batch_size: unused.
+
+    Returns:
+      an empty NestedMap.
+    """
+    return py_utils.NestedMap()
+
+  def FProp(self, theta, external_inputs, step_inputs, padding, state0):
+    """Looks up a list of embeddings from an EmbeddingLayer.
+
+    Args:
+      theta: A `.NestedMap` object containing weights' values of this layer and
+        its children layers.
+      external_inputs: unused.
+      step_inputs: A NestedMap containing a list called inputs. This list should
+        contain a single integer tensor of shape [batch], where each integer
+        represents an index into the embedding table. (By convention, all Steps
+        that can be used with StackStep must store inputs in
+        step_inputs.inputs[], but in this step it does not make sense for that
+        list to have more than one tensor in it).
+      padding: unused.
+      state0: unused.
+
+    Returns:
+      A params.dtype tensor of shape [batch, embedding_dim].
+    """
+    del external_inputs
+    del state0
+    assert len(step_inputs.inputs) == 1
+
+    output = self.emb.EmbLookup(theta.emb, step_inputs.inputs[0])
+    return py_utils.NestedMap(output=output), py_utils.NestedMap()
---
+++ lingvo/core/steps/embedding_steps_test.py
@@ -0,0 +1,61 @@
+# Lint as: python2, python3
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for lingvo.core.steps.embedding_steps."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo import compat as tf
+from lingvo.core import py_utils
+from lingvo.core import test_utils
+from lingvo.core.steps import embedding_steps
+
+
+class EmbeddingStepsTest(test_utils.TestCase):
+
+  def testEmbeddingStep(self):
+    with self.session(use_gpu=False) as sess:
+      tf.set_random_seed(398847392)
+      p = embedding_steps.EmbeddingStep.Params()
+      p.name = 'emb_step'
+      p.emb.vocab_size = 10
+      p.emb.embedding_dim = 2
+      p.emb.max_num_shards = 1
+      p.emb.params_init = py_utils.WeightInit.Gaussian(0.01)
+      p.emb.vn.global_vn = False
+      p.emb.vn.per_step_vn = False
+      emb = p.Instantiate()
+
+      # Verify that nothing bad happens when these methods are called.
+      packed = emb.PrepareExternalInputs(None, None)
+      state0 = emb.ZeroState(None, None, None)
+
+      out1, state1 = emb.FProp(
+          emb.theta, packed,
+          py_utils.NestedMap(inputs=[tf.constant([4, 3], tf.int32)]),
+          tf.constant([0.0], dtype=tf.float32), state0)
+
+      tf.global_variables_initializer().run()
+      out1, state1 = sess.run([out1, state1])
+
+      self.assertEqual({}, state1)
+      self.assertAllClose(
+          out1.output,
+          [[-5.9790569e-03, -8.7367110e-03], [-2.1643407e-06, 1.4426162e-02]])
+
+
+if __name__ == '__main__':
+  tf.test.main()
