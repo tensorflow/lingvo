d2279d78077618b230a02e2f543f147d7e208a40
Benjamin Lee <blee@google.com>
2020-02-28 21:37:03 -0800

    Wrap MLPerf subword op with a tokenizer
    PiperOrigin-RevId: 297999720

---
+++ lingvo/core/ml_perf_tokenizer.py
@@ -0,0 +1,38 @@
+# Lint as: python2, python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tokenizer for use for the MLPerf transformer benchmark."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lingvo.core import ops
+from lingvo.core import tokenizers
+
+
+class MlPerfTokenizer(tokenizers.BaseTokenizer):
+  """Id->String only for MLPerf decoding."""
+
+  @classmethod
+  def Params(cls):
+    p = super(MlPerfTokenizer, cls).Params()
+    p.Define("vocab_filepath", None, "Specifies a filepath to the vocab.")
+    return p
+
+  def IdsToStrings(self, ids, lens):
+    p = self.params
+    return ops.ml_perf_subword_id_to_string(
+        ids, lens, vocab_filepath=p.vocab_filepath)
