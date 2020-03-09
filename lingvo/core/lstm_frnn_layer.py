ac6adce5ba868e45f115781bb74001db26ce0195
James Qin <jamesqin@google.com>
2020-03-09 10:31:06 -0700

    Fast LSTM rnn layer (operating at sequence level), with input projections done in parallel.
    PiperOrigin-RevId: 299874952

---
+++ lingvo/core/lstm_frnn_layer.py
@@ -0,0 +1,233 @@
+# Lint as: python2, python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Functional LSTM RNN layer with LSTM cells fused into FRNN to run input ...
+
+projections in parallel.
+"""
+
+from lingvo import compat as tf
+from lingvo.core import base_layer
+from lingvo.core import py_utils
+from lingvo.core import recurrent
+from lingvo.core import rnn_cell
+from lingvo.core import rnn_layers
+
+
+class LSTMCellExt(object):
+  """Extends LSTM-based cell classes with extra methods for parallelizing ...
+
+  input projections across steps.
+  """
+
+  def ProjectInputSequence(self, theta, inputs):
+    """Applies input projection for the entire sequence.
+
+    Args:
+      theta: a NestedMap of layer weights. Notably, it's expected to contain
+        separate weight tensors for input and hidden state projections, for
+        performance reasons, under the key 'wm_i' (input) and 'wm_h'
+      inputs: A NestedMap with the following fields:
+        - act: A list of Tensors of shape [seqlen, batch, input_dim].
+
+    Returns:
+      A Tensor of shape [seqlen, batch, 4 * hidden_dim].
+    """
+    assert isinstance(inputs.act, list)
+    if len(inputs.act) > 1:
+      x = tf.concat(inputs.act, -1)
+    else:
+      x = inputs.act[0]
+    # [T, B, 4 * H]
+    proj_inputs = tf.einsum('TBD,DH->TBH', x, theta.wm_i)
+    return proj_inputs
+
+  def _MixWithProjectedInput(self, theta, state0, inputs):
+    """Computes _Mix() with inputs already projected.
+
+    Args:
+      theta: a NestedMap of layer weights. Notably, it's expected to contain
+        separate weight tensors for input and hidden state projections, for
+        performance reasons, under the key 'wm_i' (input) and 'wm_h'
+      state0: A NestedMap with the same structure as return value of
+        `self.zero_state()`.
+      inputs: A Tensor of shape [batch, 4 * hidden_dim].
+
+    Returns:
+      A Tensor of the same shape as `inputs`.
+    """
+    proj_m = tf.matmul(state0.m, theta.wm_h)
+    return inputs + proj_m
+
+  def FPropWithProjectedInput(self, theta, state0, inputs):
+    """FProp with inputs already projected.
+
+    This method is for parallelizing the input projection across time steps to
+    accelerate training.
+
+    The following are equivalent:
+
+    >>> inputs = <a tensor of [T, B, D]>
+    >>> paddings = tf.zeros([T, B])
+    >>> theta = cell.theta
+    >>> state = cell.zero_state(theta, B)
+
+    # a. Use FProp().
+    >>> for i in range(T):
+    ...  state, _ = cell.FProp(theta, inputs[i, :, :], paddings, state)
+
+    # b. Use FPropWithProjectedInput().
+    >>> proj_inputs = cell.ProjectInputSequence(theta, inputs)
+    >>> for i in range(T):
+    ...  state, _ = cell.FPropWithProjectedInputs(
+    ...    theta, proj_inputs[i, :, :], paddings, state)
+
+    Args:
+      theta: a NestedMap of layer weights. Notably, it's expected to contain
+        separate weight tensors for input and hidden state projections, for
+        performance reasons, under the key 'wm_i' (input) and 'wm_h' (hidden
+        state).
+      state0: A NestedMap with the same structure as return value of
+        `self.zero_state()`.
+      inputs: A NestedMap with the following fields:
+        - proj_inputs: A single Tensors of shape [batch, 4 * hidden_dim].
+        - padding: A Tensor of shape [batch, 1].
+        - reset_mask: A Tensor of shape [batch, 1].
+
+    Returns:
+      state1: A NestedMap of the same structure as `state0`.
+      extras: Intermediate results to facilitate backprop. A NestedMap.
+    """
+    if self.params.reset_cell_state:
+      state0_modified = self._ResetState(state0.DeepCopy(), inputs)
+    else:
+      state0_modified = state0
+    xmw = self._MixWithProjectedInput(theta, state0_modified,
+                                      inputs.proj_inputs)
+    gates_input = inputs.copy()
+    gates_input.act = [inputs.proj_inputs]
+    state1 = self._Gates(xmw, theta, state0_modified, gates_input)
+    return state1, py_utils.NestedMap()
+
+
+class LSTMCellSimpleExt(rnn_cell.LSTMCellSimple, LSTMCellExt):
+  """Extends LSTMCellSimple with extra methods for parallelizing ...
+
+  input projections across steps.
+  """
+  pass
+
+
+class LayerNormalizedLSTMCellLeanExt(rnn_cell.LayerNormalizedLSTMCellLean,
+                                     LSTMCellExt):
+  """Extends LayerNormalizedLSTMCellLean with extra methods for parallelizing ...
+
+  input projections across steps.
+  """
+  pass
+
+
+class LstmFRNN(base_layer.BaseLayer):
+  """A FRNN for LSTMCellSimple or LayerNormalizedLSTMCellLean cell.
+
+  It exploits the parallelism in input projection across time steps, and is in
+  general faster than the combination of LayerNormalizedLSTMCellLean and FRNN.
+  """
+
+  @classmethod
+  def Params(cls):
+    p = super(LstmFRNN, cls).Params()
+    p.Define('packed_input', False, 'To reset states for packed inputs.')
+    p.Define(
+        'cell', None,
+        'Configs for the RNN cell. Supported classes are LSTMCellSimpleExt, '
+        'LayerNormalizedLSTMCellLeanExt.')
+    return p
+
+  @base_layer.initializer
+  def __init__(self, params):
+    super(LstmFRNN, self).__init__(params)
+    p = self.params
+    if p.cell.cls not in (LSTMCellSimpleExt, LayerNormalizedLSTMCellLeanExt):
+      raise ValueError('Only LSTMCellSimpleExt and '
+                       'LayerNormalizedLSTMCellLeanExt are supported.')
+    self.CreateChild('cell', p.cell)
+
+  def zero_state(self, theta, batch_size):
+    return self.cell.zero_state(theta.cell, batch_size)
+
+  def FProp(self, theta, inputs, paddings, state0=None, segment_id=None):
+    """Computes LSTM forward pass.
+
+    Args:
+      theta: A `.NestedMap` object containing weights' values of this layer and
+        its children layers.
+      inputs: A single tensor or a tuple of tensors with cardinality equal to
+        rnn_cell.inputs_arity. For every input tensor, the first dimension is
+        assumed to be time, second dimension batch, and third dimension depth.
+      paddings: A tensor. First dim is time, second dim is batch, and third dim
+        is expected to be 1.
+      state0: If not None, the initial rnn state in a `.NestedMap`. Defaults to
+        the cell's zero-state.
+      segment_id: A tensor to support packed inputs. First dim is time, second
+        dim is batch, and third dim is expected to be 1.
+
+    Returns:
+      A tensor of [time, batch, dims].
+      The final recurrent state.
+    """
+    p = self.params
+    rcell = self.cell
+    assert isinstance(rcell, (rnn_cell.RNNCell))
+
+    if not isinstance(inputs, (list, tuple)):
+      inputs = [inputs]
+
+    # Slicing wm to wm_{i,h} outside the loop to get 20% speedup over regular
+    # LSTM baseline.
+    # Keeping slicing within the loop gives only < 3% speedup.
+    cell_theta = theta.cell.copy()
+    num_input_nodes = p.cell.num_input_nodes
+    cell_theta['wm_i'] = cell_theta.wm[:num_input_nodes, :]
+    cell_theta['wm_h'] = cell_theta.wm[num_input_nodes:, :]
+    tf.logging.vlog(1, 'cell_theta: %r', cell_theta)
+    if p.packed_input:
+      assert segment_id is not None
+      reset_mask = rnn_layers.GeneratePackedInputResetMask(
+          segment_id, is_reverse=False)
+      reset_mask = py_utils.HasShape(reset_mask, tf.shape(paddings))
+    else:
+      reset_mask = tf.zeros_like(paddings)
+
+    if not state0:
+      batch_size = py_utils.GetShape(paddings)[1]
+      state0 = rcell.zero_state(cell_theta, batch_size)
+
+    # [T, B, H]
+    proj_inputs = rcell.ProjectInputSequence(cell_theta,
+                                             py_utils.NestedMap(act=inputs))
+    proj_inputs = py_utils.NestedMap(
+        proj_inputs=proj_inputs, padding=paddings, reset_mask=reset_mask)
+
+    acc_state, final_state = recurrent.Recurrent(
+        theta=cell_theta,
+        state0=state0,
+        inputs=proj_inputs,
+        cell_fn=rcell.FPropWithProjectedInput,
+        cell_type=rcell.layer_type,
+        accumulator_layer=self,
+        allow_implicit_capture=p.allow_implicit_capture)
+
+    act = rcell.GetOutput(acc_state)
+    return act, final_state
---
+++ lingvo/core/lstm_frnn_layer_test.py
@@ -0,0 +1,217 @@
+# Lint as: python2, python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for lingvo.core.lstm_frnn_layer."""
+
+from absl.testing import parameterized
+
+from lingvo import compat as tf
+from lingvo.core import lstm_frnn_layer
+from lingvo.core import py_utils
+from lingvo.core import rnn_cell
+from lingvo.core import rnn_layers
+from lingvo.core import test_utils
+
+import numpy as np
+
+_INIT_RANDOM_SEED = 2020
+_RANDOM_SEED = 2019
+
+
+class LSTMCellExtTest(test_utils.TestCase, parameterized.TestCase):
+
+  def _GetParams(self, num_hidden_nodes=None):
+    params = lstm_frnn_layer.LayerNormalizedLSTMCellLeanExt.Params()
+    params.name = 'lstm'
+    params.output_nonlinearity = True
+    params.params_init = py_utils.WeightInit.Uniform(1.24, _INIT_RANDOM_SEED)
+    params.vn.global_vn = False
+    params.vn.per_step_vn = False
+    params.num_input_nodes = 2
+    params.num_output_nodes = 2
+    if num_hidden_nodes:
+      params.num_hidden_nodes = num_hidden_nodes
+    params.zo_prob = 0.0
+    params.random_seed = _RANDOM_SEED
+    return params
+
+  def testLayerNormalizedLSTMCellLeanExt(self):
+    cell_p = self._GetParams()
+
+    seqlen, batch, input_dim = 4, 2, 2
+    inputs = tf.convert_to_tensor(
+        np.random.rand(seqlen, batch, input_dim).astype(np.float32))
+    input_lens = np.random.randint(1, seqlen + 1, size=batch)
+    paddings = 1. - tf.sequence_mask(
+        input_lens, maxlen=seqlen, dtype=tf.float32)
+    paddings = tf.transpose(paddings)
+    reset_mask = tf.zeros((seqlen, batch), tf.float32)
+    m0 = tf.convert_to_tensor(
+        np.random.rand(batch, input_dim).astype(np.float32))
+    c0 = tf.convert_to_tensor(
+        np.random.rand(batch, input_dim).astype(np.float32))
+    state0 = py_utils.NestedMap(m=m0, c=c0)
+
+    with self.session() as sess:
+      cell = cell_p.Instantiate()
+      tf.global_variables_initializer().run()
+
+      # The canonical path
+      state = state0
+      for i in range(seqlen):
+        state, _ = cell.FPropDefaultTheta(
+            state,
+            py_utils.NestedMap(
+                act=[inputs[i, :, :]],
+                padding=paddings[i, :, tf.newaxis],
+                reset_mask=reset_mask[i, :, tf.newaxis]))
+      expected_state = sess.run(state)
+
+      # Taking input projection outside of the loop.
+      cell_theta = cell.theta.copy()
+      cell_theta.wm_i = cell_theta.wm[:cell.params.num_input_nodes, :]
+      cell_theta.wm_h = cell_theta.wm[cell.params.num_input_nodes:, :]
+      proj_inputs = cell.ProjectInputSequence(cell_theta,
+                                              py_utils.NestedMap(act=[inputs]))
+      state = state0
+      for i in range(seqlen):
+        state, _ = cell.FPropWithProjectedInput(
+            cell_theta, state,
+            py_utils.NestedMap(
+                proj_inputs=proj_inputs[i, :, :],
+                padding=paddings[i, :, tf.newaxis],
+                reset_mask=reset_mask[i, :, tf.newaxis]))
+      actual_state = sess.run(state)
+
+    tf.logging.info('expected_state:{}'.format(expected_state))
+    tf.logging.info('actual_state:{}'.format(actual_state))
+    self.assertAllClose(expected_state.m, actual_state.m)
+    self.assertAllClose(expected_state.c, actual_state.c)
+
+
+class LstmFRNNTest(test_utils.TestCase, parameterized.TestCase):
+
+  def _SetCellParams(self, cell_p, num_hidden_nodes=None):
+    params = cell_p
+    params.name = cell_p.cls.__name__
+    params.output_nonlinearity = True
+    params.params_init = py_utils.WeightInit.Uniform(1.24, _INIT_RANDOM_SEED)
+    params.vn.global_vn = False
+    params.vn.per_step_vn = False
+    params.num_input_nodes = 7
+    params.num_output_nodes = 9
+    if num_hidden_nodes:
+      params.num_hidden_nodes = num_hidden_nodes
+    params.zo_prob = 0.0
+    params.random_seed = _RANDOM_SEED
+
+  def _GetTestInputs(self, packed_input):
+    seqlen, batch, input_dim, output_dim = 4, 5, 7, 9
+    inputs = tf.convert_to_tensor(
+        np.random.rand(seqlen, batch, input_dim).astype(np.float32))
+    input_lens = np.random.randint(1, seqlen + 1, size=batch)
+    padding = 1. - tf.sequence_mask(input_lens, maxlen=seqlen, dtype=tf.float32)
+    padding = tf.transpose(padding)[:, :, tf.newaxis]
+    segment_id = None
+    if packed_input:
+      segment_id = tf.convert_to_tensor(
+          np.random.randint(0, seqlen, (seqlen, batch, 1), np.int32))
+
+    m = tf.convert_to_tensor(
+        np.random.rand(batch, output_dim).astype(np.float32))
+    c = tf.convert_to_tensor(
+        np.random.rand(batch, output_dim).astype(np.float32))
+    return inputs, padding, m, c, segment_id
+
+  def _testHelper(self, base_frnn_p, frnn_p, packed_input=False):
+    inputs, padding, m0, c0, segment_id = self._GetTestInputs(packed_input)
+    base_frnn = base_frnn_p.Instantiate()
+    frnn = frnn_p.Instantiate()
+
+    with self.session() as sess:
+      tf.global_variables_initializer().run()
+
+      state0 = py_utils.NestedMap(m=m0, c=c0)
+      act, state = base_frnn.FPropDefaultTheta(
+          inputs, padding, state0=state0, segment_id=segment_id)
+      # Compute grads
+      loss = -tf.log(
+          tf.sigmoid((tf.reduce_sum(tf.math.square(act)) +
+                      tf.reduce_sum(state.m * state.c * state.c))))
+      grads = tf.gradients(loss, base_frnn.vars.Flatten())
+
+      expected_act, expected_state, expected_grads = sess.run(
+          [act, state, grads])
+
+      act, state = frnn.FPropDefaultTheta(
+          inputs, padding, state0=state0, segment_id=segment_id)
+      # Compute grads
+      loss = -tf.log(
+          tf.sigmoid((tf.reduce_sum(tf.math.square(act)) +
+                      tf.reduce_sum(state.m * state.c * state.c))))
+      grads = tf.gradients(loss, frnn.vars.Flatten())
+
+      actual_act, actual_state, actual_grads = sess.run([act, state, grads])
+
+    tf.logging.info('expected_act:{}'.format(expected_act))
+    tf.logging.info('actual_act:{}'.format(actual_act))
+
+    tf.logging.info('expected_state:{}'.format(expected_state))
+    tf.logging.info('actual_state:{}'.format(actual_state))
+
+    tf.logging.info('expected_grads:{}'.format(expected_grads))
+    tf.logging.info('actual_grads:{}'.format(actual_grads))
+
+    self.assertAllClose(expected_act, actual_act)
+    self.assertAllClose(expected_state.m, actual_state.m)
+    self.assertAllClose(expected_state.c, actual_state.c)
+    for (vname, _), expected, actual in zip(frnn.vars.FlattenItems(),
+                                            expected_grads, actual_grads):
+      self.assertAllClose(expected, actual, msg=vname)
+
+  @parameterized.named_parameters(
+      ('HasBias', True),
+      ('NoBias', False),
+      ('HasBiasPackedInputs', True, True),
+      ('NoBiasPackedInputs', False, True),
+  )
+  def testLSTMCellSimple(self, enable_lstm_bias, packed_inputs=False):
+    base_cell_p = rnn_cell.LSTMCellSimple.Params()
+    base_cell_p.enable_lstm_bias = enable_lstm_bias
+    self._SetCellParams(base_cell_p)
+    base_frnn_p = rnn_layers.FRNN.Params().Set(
+        name='base_frnn', cell=base_cell_p)
+
+    cell_p = lstm_frnn_layer.LSTMCellSimpleExt.Params()
+    cell_p.enable_lstm_bias = enable_lstm_bias
+    self._SetCellParams(cell_p)
+    frnn_p = lstm_frnn_layer.LstmFRNN.Params().Set(name='frnn', cell=cell_p)
+    self._testHelper(base_frnn_p, frnn_p, packed_inputs)
+
+  def testLayerNormalizedLSTMCellLean(self):
+    base_cell_p = rnn_cell.LayerNormalizedLSTMCellLean.Params()
+    base_cell_p.enable_lstm_bias = True
+    self._SetCellParams(base_cell_p)
+    base_frnn_p = rnn_layers.FRNN.Params().Set(
+        name='base_frnn', cell=base_cell_p)
+
+    cell_p = lstm_frnn_layer.LayerNormalizedLSTMCellLeanExt.Params()
+    cell_p.enable_lstm_bias = True
+    self._SetCellParams(cell_p)
+    frnn_p = lstm_frnn_layer.LstmFRNN.Params().Set(name='frnn', cell=cell_p)
+    self._testHelper(base_frnn_p, frnn_p)
+
+
+if __name__ == '__main__':
+  tf.test.main()
