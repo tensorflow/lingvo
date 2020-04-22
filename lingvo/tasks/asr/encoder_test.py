f042e0c995e8f553c5bec06fd46090a667086df5
Lingvo Maintenance <lingvo-bot@google.com>
2020-04-21 20:14:43 -0700

    Convert tests to Python 3.
    PiperOrigin-RevId: 307733246

--- lingvo/tasks/asr/encoder_test.py
+++ lingvo/tasks/asr/encoder_test.py
@@ -1,4 +1,4 @@
-# Lint as: python2, python3
+# Lint as: python3
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for ASR encoder."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import lingvo.compat as tf
 from lingvo.core import py_utils
--- lingvo/tasks/mt/decoder_test.py
+++ lingvo/tasks/mt/decoder_test.py
@@ -1,4 +1,4 @@
-# Lint as: python2, python3
+# Lint as: python3
 # Copyright 2018 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,9 +14,6 @@
 # limitations under the License.
 """Tests for mt.decoder."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 from absl.testing import parameterized
 import lingvo.compat as tf
