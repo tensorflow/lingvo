0dede8e5635e5c1004225af71c4b3cd2dba80f19
Benjamin Lee <blee@google.com>
2020-05-19 14:16:45 -0700

    Move Builder pattern into Lingvo
    PiperOrigin-RevId: 312351252

---
+++ lingvo/core/builder.py
@@ -0,0 +1,320 @@
+# Lint as: python2, python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""A library to build composite layers.
+
+WARNING:
+  The builder pattern is still experimental and we need to gain experience
+  on when to use and when not to use.
+  Please discuss w/ teammates before using it to build complicated
+  layers.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+from lingvo import compat as tf
+from lingvo.core import builder_layers
+from lingvo.core import hyperparams
+from lingvo.core import layers
+from lingvo.core import py_utils
+from lingvo.core import tshape
+from six.moves import range
+from six.moves import zip
+
+
+class Base(object):
+  """Model builder with commonly used layers.
+
+  A method in a builder class constructs a layer param.  FProp of a layer
+  constructed by a builder takes a tuple of tf.Tensor (one or more) and returns
+  a tuple of tf.Tensor (one or more). Even though certain layers support FProp
+  argument being None (e.g., Conv2DLayer), builder should not depend on such a
+  support.
+
+  The constructed layer is often a composition of multiple sub-layers connected
+  in certain patterns. We expect to have a few methods to facilitate building
+  these patterns. For example, _Seq() helps to build a sequential layer that
+  calls its sub-layer one after another.
+
+  TODO(zhifengc): Adds a more concrete example.
+  """
+
+  @classmethod
+  def Params(cls):
+    """The params of this layer."""
+    p = hyperparams.InstantiableParams(cls)
+    p.Define('deterministic_dropout', False,
+             'Used deterministic dropout or not.')
+    p.Define(
+        'fprop_dtype', None,
+        'Activations datatype to use. To enable bfloat16 activations for '
+        'layers built using model builder, set fprop_dtype to '
+        'tf.bfloat16, which will be propagated to layers that support '
+        'bfloat16 activations. Default is None, which will use float32 '
+        'activations.')
+    return p
+
+  @property
+  def params(self):
+    """Returns the params upon which this layer is built."""
+    return self._params
+
+  def __init__(self, params):
+    # Sub-classes should put some options common to many layers in __init__.
+    self._params = params.Copy()
+
+  ######################################################################
+  # Layers to compose multiple layers.
+  #
+  # Sub-classes are discouraged to override these composition method.
+  ######################################################################
+  def _Rep(self, name, repeat, *subs):
+    r"""Connects sub-layers sequentially and repeat multiple times.
+
+    E.g., _Rep('foo', 2, sa, sb, sc) constructs a layer with 6 layers
+    sequentially connected: [sa1, sb1, sc1, sa2, sb2, sc2].  sa1 and sa2 have
+    the same structure as the given sa, but sa1 and sa2 do not share the same
+    weight.
+
+    Args:
+      name: The layer name.
+      repeat: Repeat \*subs this many times in the compose layer.
+      *subs: A list of sub-layers.
+
+    Returns:
+      The param for the composed layer.
+    """
+    iterations = []
+    for i in range(repeat):
+      iterations.append(self._Seq('iter_%03d' % i, *[p.Copy() for p in subs]))
+    return self._Seq(name, *iterations)
+
+  def _Seq(self, name, *subs):
+    """Connects sub-layers sequentially."""
+    return builder_layers.SequentialLayer.Params().Set(
+        name=name, sub=list(subs))
+
+  def _Graph(self, name, input_endpoints, output_endpoints,
+             *signature_sub_param_list):
+    """Connects sub-layers into a data flow graph."""
+    return builder_layers.GraphLayer.Params().Set(
+        name=name,
+        input_endpoints=input_endpoints,
+        output_endpoints=output_endpoints,
+        sub=list(signature_sub_param_list))
+
+  def _Id(self, name):
+    """Identity. (t_1, ..., t_n) -> (t1, ..., t_n)."""
+    return self._Seq(name)
+
+  def _Arg(self, name, index):
+    """Picks index-th element. (t_1, ..., t_n) -> (t_{index},)."""
+    return builder_layers.ArgIndexLayer.Params().Set(name=name, idx=[index])
+
+  def _Par(self, name, *subs):
+    """y = (f1, f2, ..., fn)(x).
+
+    We feed the input tuple to all sub-layers and concatenates their output
+    tuples into one tuple.
+
+    Args:
+      name: The layer name.
+      *subs: A list of sub-layers.
+
+    Returns:
+      The param for the composed layer.
+    """
+
+    def ConcatTuples(tuples):
+      # tuples is a list of tuples.
+      return tuple(functools.reduce(lambda x, y: x + list(y), tuples, []))
+
+    def ConcatMeta(tuples):
+      return py_utils.NestedMap(
+          flops=0,
+          out_shapes=tuple(
+              functools.reduce(lambda x, y: x + list(y), tuples, [])))
+
+    return builder_layers.ParallelLayer.Params().Set(
+        name=name, sub=list(subs), merge=ConcatTuples, merge_meta=ConcatMeta)
+
+  def _Fn(self, name, fn, fn_out=None, fn_flops=None):
+    """y = fn(x).
+
+    Applies a fn: tuple(Tensor) -> a single Tensor or tuple(Tensor) to the input
+    tuple.  Typically, fn is a very simple python function. If fn is rather
+    complicated, we advice to implement the logic as a sub-class of BaseLayer.
+
+    Args:
+      name: The layer name.
+      fn: A lambda tuple(Tensor) -> tuple(Tensor).
+      fn_out: A lambda tuple(tshape.Shape) -> output tuple(tshape.Shape)
+      fn_flops: A lambda tuple(tshape.Shape) -> estimated flops of fn.
+        If None, we assume flops == sum of elements in the inputs.
+
+    Returns:
+      The param for the composed layer.
+    """
+
+    def FnMeta(*shapes):
+      """A lambda tuple(tshape.Shape) -> NestedMap{flops, out_shapes}."""
+      if fn_out:
+        out_shapes = fn_out(*shapes)
+        if isinstance(out_shapes, tshape.Shape):
+          out_shapes = (out_shapes,)
+      else:
+        out_shapes = shapes
+      if fn_flops:
+        flops = fn_flops(*shapes)
+      else:
+        flops = sum([s.size for s in shapes])
+      return py_utils.NestedMap(flops=flops, out_shapes=out_shapes)
+
+    return builder_layers.FnLayer.Params().Set(name=name, fn=fn, fn_meta=FnMeta)
+
+  def _Save(self, name):
+    """Returns a layer from which the activation and gradient can be accessed."""
+    return layers.FetchLayer.Params().Set(name=name)
+
+  def _AddFetches(self, name, body, fetches):
+    """Fetches saved activations in the body sub-layer.
+
+    E.g.:
+    _AddFetches('foo', _Seq( 'stack', _Layer('layer1', ...),
+    _Save('layer1_out', ...), _Layer('layer2', ...), _Save('layer2_out', ...),
+    _Output('output', ...)), ['layer1_out', 'layer2_out'])
+
+    The layer returns the stack's final output together with intermediate
+    activations from layer1_out and layer2_out.
+
+    Args:
+      name: This layer's name.
+      body: The sub-layer.
+      fetches: A list of fetch names inside the sub-layer body.
+
+    Returns:
+      A layer whose outputs correspond to the activations of fetch points
+      in the sub-layer body. [input1, input2, ..., inputN, fetch1, ..., fetchM].
+    """
+    return builder_layers.BranchLayer.Params().Set(
+        name=name, body=body, fetches=fetches)
+
+  def _Rematerialize(self, name, body):
+    """Forces rematerialization on FProp of the body layer."""
+    return builder_layers.RematerializationLayer.Params().Set(
+        name=name, body=body)
+
+  def _BatchParallel(self, name, sub):
+    """Splits the batch and compute the forward pass on multiple devices.
+
+    Args:
+      name: This layer's name.
+      sub: The sub-layer.
+
+    Returns:
+      A BatchParallel layer which splits the batch and computes the forward pass
+      on multiple devices.
+    """
+    return builder_layers.BatchParallelLayer.Params().Set(name=name, sub=sub)
+
+  def _PrintShape(self, name):
+    """Print FProp input shape information."""
+    return builder_layers.PrintShapeLayer.Params().Set(name=name)
+
+  ###########################################################################
+  # Basic nn layers.
+  #
+  # The following method returns a layer param, whose FProp takes a single
+  # Tensor and returns a single Tensor.
+  #
+  # These methods are designed to have minimal knobs. Sub-classes which needs to
+  # be flexible can override these methods with different options. E.g., a
+  # sub-class builder can override _BN() to tune the decay option.
+  ###########################################################################
+  def _BN(self, name, dims):
+    """Batch norm."""
+    return layers.BatchNormLayer.Params().Set(name=name, dim=dims, decay=0.99)
+
+  def _LN(self, name, dims, use_fused_layernorm=False):
+    """Layer norm."""
+    return layers.LayerNorm.Params().Set(
+        name=name,
+        input_dim=dims,
+        use_fused_layernorm=use_fused_layernorm,
+        fprop_dtype=self.params.fprop_dtype)
+
+  def _Dropout(self, name, keep_prob, noise_shape_broadcast_dims=None):
+    """Returns a DropoutLayer Params."""
+    if self.params.deterministic_dropout:
+      return layers.DeterministicDropoutLayer.Params().Set(
+          name=name,
+          keep_prob=keep_prob,
+          noise_shape_broadcast_dims=noise_shape_broadcast_dims)
+    return layers.DropoutLayer.Params().Set(
+        name=name,
+        keep_prob=keep_prob,
+        noise_shape_broadcast_dims=noise_shape_broadcast_dims,
+        fprop_dtype=self.params.fprop_dtype)
+
+  def _Linear(self, name, idims, odims):
+    """Linear layer. y = matmul([..., idims], [idims, odims])."""
+    return builder_layers.LinearLayer.Params().Set(
+        name=name,
+        input_dims=idims,
+        output_dims=odims,
+        fprop_dtype=self.params.fprop_dtype)
+
+  def _Bias(self, name, dims):
+    """Bias layer. The bias is added to the last dimension of the input."""
+    return builder_layers.BiasLayer.Params().Set(
+        name=name, dims=dims, fprop_dtype=self.params.fprop_dtype)
+
+  def _Activation(self, name, fn=tf.nn.relu):
+    """Activation layer."""
+    return self._Fn(name, fn)
+
+  def _FC(self, name, idims, odims):
+    """Feed-forward fully connected. y = relu(matmul(x, w) + b)."""
+    # pyformat: disable
+    return self._Seq(
+        name,
+        self._Linear('linear', idims, odims),
+        self._Bias('bias', odims),
+        self._Activation('act'))
+
+  def _MLP(self, name, dims):
+    """Multiple layers of feed-forward fully connected.
+
+    Args:
+      name: The layer name.
+      dims: A list of int. i-th layer has dims[i] as its input dimension, and
+        dims[i+1] as its output dimensions.
+
+    Returns:
+      The param for the composed layer.
+    """
+    l = []
+    for n, (i, o) in enumerate(zip(dims[:-1], dims[1:])):
+      l += [self._FC('l%03d' % n, i, o)]
+    return self._Seq(name, *l)
+
+  def _Conv2D(self, name, filter_shape, filter_stride):
+    """Conv2D layer."""
+    return layers.Conv2DLayerNoPadding.Params().Set(
+        name=name, filter_shape=filter_shape, filter_stride=filter_stride,
+        fprop_dtype=self.params.fprop_dtype)
---
+++ lingvo/core/builder_test.py
@@ -0,0 +1,337 @@
+# Lint as: python3
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for the Builder pattern."""
+
+import inspect
+from lingvo import compat as tf
+from lingvo.core import builder
+from lingvo.core import cluster_factory
+from lingvo.core import layers
+from lingvo.core import py_utils
+from lingvo.core import test_utils
+from lingvo.core import tshape
+import numpy as np
+from six.moves import range
+import sympy
+
+
+class BuilderTest(test_utils.TestCase):
+
+  def _Expect(self, expected_cost, p, *inputs):
+    meta = p.cls.FPropMeta(p, *(tshape.Shape(s) for s in inputs))
+    self.assertEqual(meta.flops, expected_cost)
+
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      xs = [tf.random.normal(shape=s) for s in inputs]
+      ys = l.FPropDefaultTheta(*xs)
+
+    with self.session(graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      _ = sess.run(ys)
+
+  def testCostEstimate(self):
+    b = builder.Base.Params()
+    b = b.Instantiate()
+    # pyformat: disable
+    self._Expect(2400, b._Linear('l', 10, 20), [2, 3, 10])
+    self._Expect(60, b._Bias('b', 10), [2, 3, 10])
+    self._Expect(150, b._Activation('a'), [3, 5, 10])
+    self._Expect(600, b._BN('b', 10), [2, 3, 10])
+    self._Expect(
+        (2**21) * 9 * 2,
+        b._Conv2D('c', (3, 3, 8, 16), (1, 1)),
+        [16, 32, 32, 8])
+    self._Expect(
+        (2**19) * 9 * 2,
+        b._Conv2D('c', (3, 3, 8, 16), (2, 2)),
+        [16, 32, 32, 8])
+    self._Expect(
+        (2**20) * 9 * 2,
+        b._Conv2D('c', (3, 3, 8, 16), (1, 2)),
+        [16, 32, 32, 8])
+    self._Expect(
+        24 * 10 * 10 * 2 * 7,
+        b._Rep(
+            'rep',
+            7,
+            b._Linear('l', 10, 10)),
+        [8, 3, 10])
+    self._Expect(
+        (2**21) * 9 * 2 + (2**14) * 16 * 32 * 2 + (2**14) * 32,
+        b._Seq(
+            'seq',
+            b._Conv2D('c', (3, 3, 8, 16), (1, 1)),
+            b._Linear('l', 16, 32),
+            b._Activation('a')),
+        [16, 32, 32, 8])
+    self._Expect(
+        (((2**19) * 9 * 2 + (2**12) * 16 * 32 * 2 + (2**12) * 32) * 2),
+        b._Par(
+            'p',
+            b._Seq(
+                'b0',
+                b._Conv2D('c', (3, 3, 8, 16), (2, 2)),
+                b._Linear('l', 16, 32),
+                b._Activation('a')),
+            b._Seq(
+                'b1',
+                b._Conv2D('c', (3, 3, 8, 16), (2, 2)),
+                b._Linear('l', 16, 32),
+                b._Activation('a'))),
+        [16, 32, 32, 8])
+    # pyformat: enable
+
+  def testFetch(self):
+    # Construct a layer w/ two parallel branches.  We want to demonstrate that
+    # we can fetch intermediate values from two branches independently.
+    # pyformat: disable
+    b = builder.Base.Params()
+    b = b.Instantiate()
+    p = b._Par(
+        'p',
+        b._Seq(
+            'b0',
+            b._Conv2D('c', (3, 3, 8, 16), (2, 2)),
+            b._Linear('l', 16, 32),
+            b._Save('fetch'),
+            b._Activation('a')),
+        b._Seq(
+            'b1',
+            b._Conv2D('c', (3, 3, 8, 16), (2, 2)),
+            b._Linear('l', 16, 32),
+            b._Activation('a'),
+            b._Save('fetch')))
+    p = b._AddFetches('mh', p, ['b0.fetch', 'b1.fetch'])
+    # pyformat: enable
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[17, 64, 64, 8])
+      outs = l.FPropDefaultTheta(x)
+      x, y, b0, b1 = outs
+
+    with self.session(graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      v0, v1, u, v = sess.run([b0, b1, x, y])
+
+    self.assertAllClose(np.maximum(0, v0), u)
+    self.assertAllClose(v1, v)
+
+  def testFetchGrad(self):
+    # Tests we can fetch backprop gradients.
+    # pyformat: disable
+    b = builder.Base.Params()
+    b = b.Instantiate()
+    p = b._Seq(
+        'seq',
+        b._Linear('l', 16, 32),
+        b._Bias('b', 32),
+        b._Save('fetch'),
+        b._Activation('a'))
+    # pyformat: enable
+
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[4, 16])
+      y = l.FPropDefaultTheta(x)
+      loss = tf.reduce_sum(tf.square(y))
+      _ = tf.gradients(ys=loss, xs=x)
+
+      act, dact = l.fetch.activation, l.fetch.gradient
+
+    with self.session(graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      act_v, dact_v = sess.run([act, dact])
+
+    # The last layer two layers is sum(square(relu(act))).
+    # So the dact is simply 2*relu(act).
+    self.assertAllClose(2 * np.maximum(0, act_v), dact_v)
+
+  def testBatchParallel(self):
+    # pyformat: disable
+    b = builder.Base.Params()
+    b = b.Instantiate()
+    p = b._BatchParallel(
+        'bp',
+        b._Seq(
+            'main',
+            b._Linear('l', 8, 4),
+            b._PrintShape('debug'),
+            b._Bias('b', 4)))
+    # pyformat: enable
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[4, 8])
+      with cluster_factory.ForTestingWorker(cpus=4, split_size=1):
+        y1 = l.FPropDefaultTheta(x)
+      with cluster_factory.ForTestingWorker(cpus=4, split_size=2):
+        y2 = l.FPropDefaultTheta(x)
+      with cluster_factory.ForTestingWorker(cpus=4, split_size=4):
+        y4 = l.FPropDefaultTheta(x)
+
+    cfg = tf.config_pb2.ConfigProto()
+    cfg.device_count['CPU'] = 4
+    with self.session(config=cfg, graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      v1, v2, v4 = sess.run([y1, y2, y4])
+
+    self.assertAllClose(v1, v2)
+    self.assertAllClose(v1, v4)
+
+  def testFn(self):
+    b = builder.Base.Params()
+    b = b.Instantiate()
+
+    p = b._Fn('fn', lambda x, y: x + y, fn_out=lambda x, y: x)
+
+    meta = p.cls.FPropMeta(p, tshape.Shape([4, 6]), tshape.Shape([4, 6]))
+    self.assertEqual(meta.flops, 48)
+    self.assertEqual(meta.out_shapes[0].ToTensorShape().as_list(), [4, 6])
+
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[4, 8])
+      y = tf.random.normal(shape=[4, 1])
+      z = l.FPropDefaultTheta(x, y)
+
+    with self.session(graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      v = sess.run([x, y, z])
+
+    self.assertAllClose(v[0] + v[1], v[2])
+
+  def testRematerialize(self):
+    # Test the dropout consistency between fprop and bprop.
+    b = builder.Base.Params()
+    b = b.Instantiate()
+    start_block = layers.DeterministicDropoutLayer.Params().Set(
+        name='start_dropout', keep_prob=0.7)
+    # Build 4 dropout layers, each wrapped by RematerializeFn.
+    num_blocks = 4
+    blocks = []
+    blocks_per_cell = 2
+    for i in range(num_blocks):
+      blocks.append(layers.DeterministicDropoutLayer.Params().Set(
+          name='dropout_{}'.format(i), keep_prob=0.7))
+    cells = []
+    while blocks:
+      heads, blocks = blocks[:blocks_per_cell], blocks[blocks_per_cell:]
+      cell_name = 'cell_{}'.format(len(cells))
+      cells.append(
+          b._Rematerialize(name=cell_name, body=b._Seq(cell_name, *heads)))
+    with self.session(use_gpu=False, graph=tf.Graph()) as sess:
+      tf.random.set_seed(12345)
+      p = b._Seq('test', start_block, *cells)
+      mdl = p.Instantiate()
+      # y = mdl.Frop(x * w)
+      # Fake input
+      x = tf.ones([4, 5])
+      # Construct weights.
+      w = tf.get_variable(
+          'w', shape=[4, 5], initializer=tf.constant_initializer([[1] * 5] * 4))
+      y = mdl.FPropDefaultTheta(x * w)
+      # Construct loss function such that gradients = final activation.
+      # dy/dw = y = mdl.Frop(x * w) when w is 1.
+      loss = tf.reduce_sum(y)
+      grads = py_utils.ComputeGradients(loss, py_utils.NestedMap(w=w))
+      tf.global_variables_initializer().run()
+      y_val, grads_val = sess.run([y, grads.Transform(tuple)])
+      grads_val = grads_val['w'][1]
+      self.assertAllClose(y_val, grads_val)
+      self.assertEqual(py_utils.GetStepSeed().eval(), 441070709)
+
+  def testFnDefaultMeta(self):
+    b = builder.Base.Params()
+    b = b.Instantiate()
+
+    def Foo(x, y):
+      return x * x, y * 2
+
+    p = b._Fn('fn', Foo)
+
+    meta = p.cls.FPropMeta(p, tshape.Shape([4, 6]), tshape.Shape([3, 3]))
+    self.assertEqual(meta.flops, 33)
+    self.assertEqual(meta.out_shapes[0].ToTensorShape().as_list(), [4, 6])
+    self.assertEqual(meta.out_shapes[1].ToTensorShape().as_list(), [3, 3])
+
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[4, 8])
+      y = tf.random.normal(shape=[3, 3])
+      z0, z1 = l.FPropDefaultTheta(x, y)
+
+    with self.session(graph=g) as sess:
+      sess.run(tf.global_variables_initializer())
+      vx, vy, vz0, vz1 = sess.run([x, y, z0, z1])
+
+    self.assertAllClose(vx * vx, vz0)
+    self.assertAllClose(vy * 2, vz1)
+
+  def testSymbolicDims(self):
+    p = builder.Base.Params()
+    b = p.Instantiate()
+
+    f1 = tshape.Shape(['kh', 'kw', 'idims', 'odims'])
+    kh, kw, idims, odims = f1
+    f2 = tshape.Shape([kh, kw, odims, odims])
+    p = b._Seq('test', b._Conv2D('conv', f1, (2, 2)),
+               b._Conv2D('conv', f2, (2, 2)), b._Bias('bias', odims))
+
+    inp = tshape.Shape(['b', 'h', 'w', idims])
+    b, h, w, _ = inp
+    meta = p.cls.FPropMeta(p, inp)
+    print('flops = ', meta.flops)
+    out = meta.out_shapes[0]
+    print('outputs = ', out)
+
+    # sympy.lambdify can help us to do faster numerical evaluation.
+    # Might be useful to build a "cost" model given a builder layer.
+    f = sympy.lambdify([b, h, w, kh, kw, idims, odims], meta.flops, 'numpy')
+    print('f.source = ', inspect.getsource(f))
+    self.assertEqual(f(8, 224, 224, 3, 3, 8, 32), 925646848)
+    self.assertEqual(f(8, 224, 224, 5, 5, 8, 32), 2569814016)
+
+  def testDoEval(self):
+    p = builder.Base.Params().Instantiate()._Dropout('dropout', 0.5)
+
+    g = tf.Graph()
+    with g.as_default():
+      l = p.Instantiate()
+      x = tf.random.normal(shape=[16, 16])
+      # FProp three times each with different do_eval mode.
+      with cluster_factory.SetEval(mode=None):
+        a = l.FPropDefaultTheta(x)
+      with cluster_factory.SetEval(mode=False):
+        b = l.FPropDefaultTheta(x)
+      with cluster_factory.SetEval(mode=True):
+        c = l.FPropDefaultTheta(x)
+
+    with self.session(graph=g) as sess:
+      x, a, b, c = sess.run([x, a, b, c])
+
+    self.assertGreater(np.linalg.norm(x - a), 0)
+    self.assertGreater(np.linalg.norm(x - b), 0)
+    self.assertAllEqual(x, c)
+
+
+if __name__ == '__main__':
+  tf.test.main()
