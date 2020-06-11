366ee733658a81313e2f826665f134f30077407f
Lingvo Maintenance <lingvo-bot@google.com>
2020-06-11 11:06:11 -0700

    Add DefineParamsFromFunctionArgs and CallFunctionWithParams.
    These functions allow you to save parameter settings for
    function and constructor calls without having to write a lot
    of boilerplate code.
    
    PiperOrigin-RevId: 315936197

--- lingvo/core/hyperparams.py
+++ lingvo/core/hyperparams.py
@@ -196,6 +196,98 @@ def CopyParamsTo(from_p, to_p, skip=None):
   return to_p
 
 
+def DefineParamsFromArgs(func, params, ignore=None):
+  """Defines params for each parameter of func.
+
+  This allows you to define the parameters necessary to call a function
+  or instantiate a class without having to type the Define statements yourself.
+  Default values for the function parameters will be copied into the params
+  object as well.
+
+  To use this with a class constructor, use MyClass.__init__ as the func
+  parameter.
+
+  Args:
+    func: A function or method. Parameters for this function will be
+    params: A hyperparams object. New parameters will be defined here.
+    ignore: Don't define parameters in the params object for these function
+      parameters.
+  """
+  # Get the call signature of the function.
+  init_signature = inspect.signature(func)
+  for parameter in init_signature.parameters.values():
+    if parameter.name in ['self', 'args', 'kwargs']:
+      continue
+    if ignore and parameter.name in ignore:
+      continue
+    # Add each parameter of the constructor to the params object.
+    # If the parameter has a default, add that too.
+    params.Define(parameter.name, parameter.default, 'Function parameter.')
+
+
+def _ExtractCallParams(func, params, **kwargs):
+  """Extracts parameters from params that should be used to call func.
+
+  Args:
+    func: A function, constructor, or method.
+    params: A hyperparams object containing arguments for func.
+    **kwargs: Argument/value pairs that should override params.
+
+  Returns:
+    A dict containing function parameters.
+  """
+  # Read the list of parameters that func requires.
+  init_signature = inspect.signature(func)
+  func_params = {}
+  for parameter in init_signature.parameters.values():
+    key = parameter.name
+    # Anything in kwargs overrides parameters.
+    if key in kwargs:
+      func_params[key] = kwargs[key]
+      continue
+    # If there's something in the function signature that's not in params,
+    # skip it.
+    if key not in params:
+      continue
+    # These are special function parameters that we should skip.
+    if key in ['self', 'args', 'kwargs']:
+      continue
+    # If the value in params is the same as the function default, we will
+    # let the function signature fill in this parameter for us.
+    if init_signature.parameters[key].default == params.Get(key):
+      continue
+    func_params[key] = params.Get(key)
+  return func_params
+
+
+def CallWithParams(func, params, **kwargs):
+  """Call a function or method with a hyperparams object.
+
+  Args:
+    func: A function or method.
+    params: A hyperparams object with parameters to pass to func.
+    **kwargs: Argument/value pairs that should override params.
+
+  Returns:
+    The return values from func.
+  """
+  return func(**_ExtractCallParams(func, params, **kwargs))
+
+
+def ConstructWithParams(class_type, params, **kwargs):
+  """Construct and object with a hyperparams object.
+
+  Args:
+    class_type: A class type.
+    params: A hyperparams object with parameters to pass to the constructor.
+    **kwargs: Argument/value pairs that should override params.
+
+  Returns:
+    The constructed object.
+  """
+  return class_type(**_ExtractCallParams(class_type.__init__, params, **kwargs))
+
+
 class Params(object):
   """Stores data for a set of parameters.
 
