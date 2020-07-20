Fixes FlipY preprocessor augmentation so heading is correct (instead of rotated 180 degrees). This only affects rotation aware models.
PiperOrigin-RevId: 322154346

lingvo/tasks/car/input_preprocessors.py
@@ -2207,7 +2207,7 @@ class RandomFlipY(Preprocessor):
     # Compensate rotation.
     bboxes_dims = features.labels.bboxes_3d[..., 3:6]
     bboxes_rot = features.labels.bboxes_3d[..., 6:]
-    bboxes_rot = tf.where(choice, geometry.WrapAngleRad(-bboxes_rot + np.pi),
+    bboxes_rot = tf.where(choice, geometry.WrapAngleRad(-bboxes_rot),
                           bboxes_rot)
     features.labels.bboxes_3d = tf.concat([bboxes_xyz, bboxes_dims, bboxes_rot],
                                           axis=-1)