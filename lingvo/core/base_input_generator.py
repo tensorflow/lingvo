391780f18fa87bc12b726654be2d98664de519d7
Lingvo Maintenance <lingvo-bot@google.com>
2020-07-31 17:10:15 -0700

    Add per_core_infeed option to allow handling of per host infeed with data parallelism while num_partitions is > 1.
    PiperOrigin-RevId: 324313735

--- lingvo/core/base_input_generator.py
+++ lingvo/core/base_input_generator.py
@@ -59,6 +59,38 @@ class BaseInputGenerator(base_layer.BaseLayer):
   """The abstract base input generator."""
 
   @classmethod
+  def DefineInfeedParams(cls, p):
+    # TPU related infeed tuning.
+    # Supported use cases:
+    #
+    # Data parallelism (num_partitions=None)
+    #  - single host (use_per_host_infeed=False, tpu_infeed_parallelism=1))
+    #  - multi host (use_per_host_infeed=False, tpu_infeed_parallelism>1)
+    #  - per host (use_per_host_infeed=True)
+    # Model parallelism (num_partitions>1 where)
+    #  - non-partitioned infeed (use_partitioned_infeed_queue=False):
+    #    - Only first partition gets infeed (e.g. manual partition)
+    #      - single host (use_per_host_infeed=False)
+    #      - per host (use_per_host_infeed=True)
+    #    - All partitions gets data parallel infeed (e.g. MoE)
+    #      - single host not supported
+    #      - per host (use_per_host_infeed=True, use_per_core_infeed=True)
+    #        num_partitions should be set to number of partitions per replica
+    #  - partitioned infeed (use_partitioned_infeed_queue=True)
+    #    - single host (use_per_host_infeed=False)
+    #    - per host (use_per_host_infeed=True)
+    #        num_partitions should be set to number of partitions per replica
+    #        and all partitions should exist on a single host
+    p.Define('use_per_host_infeed', False,
+             'Whether run infeed op on each host.')
+    p.Define('use_per_core_infeed', False,
+             'Whether to shard the infeed per TPU core instead of per replica')
+    p.Define('tpu_infeed_parallelism', 1,
+             'Uses these many python threads to drive infeed concurrently.')
+    p.Define('use_partitioned_infeed_queue', False, 'Use partitioned infeed')
+    p.Define('num_partitions', None, 'Num partitions')
+
+  @classmethod
   def Params(cls):
     """Defaults params for input generators."""
     p = super().Params()
@@ -72,14 +104,7 @@ class BaseInputGenerator(base_layer.BaseLayer):
         'For test/eval dataset, if we want the test/evel job evaluate '
         'the whole dataset, this param must be set precisely. Otherwise, '
         'this param is optional.')
-
-    # TPU related infeed tuning.
-    p.Define('use_per_host_infeed', False,
-             'Whether run infeed op on each host.')
-    p.Define('tpu_infeed_parallelism', 1,
-             'Uses these many python threads to drive infeed concurrently.')
-    p.Define('use_partitioned_infeed_queue', False, 'Use partitioned infeed')
-    p.Define('num_partitions', None, 'Num partitions')
+    cls.DefineInfeedParams(p)
 
     p.Define('remote', hyperparams.Params(),
              'Params to configure remote input policy.')
@@ -178,8 +203,9 @@ class BaseInputGenerator(base_layer.BaseLayer):
     cluster = self.cluster
     num_tpu_hosts = cluster.num_tpu_hosts
     num_infeed_hosts = num_tpu_hosts if p.use_per_host_infeed else 1
-    shards = (cluster.total_worker_devices //
-              num_infeed_hosts) // cluster.num_devices_per_split
+    shards = (cluster.total_worker_devices // num_infeed_hosts)
+    if p.use_partitioned_infeed_queue or not p.use_per_core_infeed:
+      shards = shards // cluster.num_devices_per_split
     return shards
 
   def CreateTpuEnqueueOps(self):
@@ -201,6 +227,10 @@ class BaseInputGenerator(base_layer.BaseLayer):
                 num_tpu_hosts, p.use_per_host_infeed))
 
     assert num_tpu_hosts > 0, ('num_tpu_hosts: %d' % num_tpu_hosts)
+    if p.use_per_core_infeed:
+      if (not p.use_per_host_infeed) or p.use_partitioned_infeed_queue:
+        raise ValueError('use_per_core_infeed need to have use_per_host_infeed '
+                         'but not use_partitioned_infeed_queue.')
     if (cluster.num_devices_per_split > num_cores_per_host and
         p.use_per_host_infeed):
       tf.logging.fatal('Doesn\'t support per host infeed mode when '
@@ -274,7 +304,13 @@ class BaseInputGenerator(base_layer.BaseLayer):
               tuple_types=dtypes,
               tuple_shapes=shapes)
         else:
-          q = tpu_feed.InfeedQueue(tuple_types=dtypes, tuple_shapes=shapes)
+          if p.use_per_core_infeed:
+            q = tpu_feed.InfeedQueue(
+                tuple_types=dtypes,
+                tuple_shapes=shapes,
+                number_of_partitions=p.num_partitions)
+          else:
+            q = tpu_feed.InfeedQueue(tuple_types=dtypes, tuple_shapes=shapes)
           assert shards is not None
           q.set_number_of_shards(shards)
 
@@ -285,6 +321,8 @@ class BaseInputGenerator(base_layer.BaseLayer):
         elif p.use_per_host_infeed:
           # TODO(ylc/zhifengc): Add this to a policy module and test it.
           def TPUOrdinalFunction(shard_index_in_host):
+            if p.use_per_core_infeed:
+              return shard_index_in_host
             device_assignment = py_utils.GetTpuDeviceAssignment()
             if device_assignment:
               # We put both enqueue/dequeue ops at core 0 in each replica.
