ec4fcc639c8154b21ab20f2660a93b3ddaf408b4
Benjamin Lee <blee@google.com>
2020-11-25 14:00:14 -0800

    Workaround for GatherV2/ResourceGather issue.
    PiperOrigin-RevId: 344312751

--- lingvo/core/layers.py
+++ lingvo/core/layers.py
@@ -2237,13 +2237,19 @@ class SimpleEmbeddingLayer(quant_utils.QuantizableLayer):
 
     def EmbGather(xs):
       """Lookups embedding vectors."""
-      # If tf.gather is used, the gradient for the wm will be represented as
-      # IndexedSlices which is sparse. tf.tpu.cross_replica_sum turns
-      # IndexedSlices into a dense tensor with undefined first dimension.
-      # This may cause issues on TPU so instead we just wrap this with
-      # tf.identity which allows tf.tpu.cross_replica_sum to properly compute
-      # the first dim.
-      return tf.nn.embedding_lookup(tf.identity(xs.embs), xs.ids_vec)
+      if not self.do_eval:
+        # If tf.gather is used, the gradient for the wm will be represented as
+        # IndexedSlices which is sparse. tf.tpu.cross_replica_sum turns
+        # IndexedSlices into a dense tensor with undefined first dimension.
+        # This may cause issues on TPU so instead we just wrap this with
+        # tf.identity which allows tf.tpu.cross_replica_sum to properly compute
+        # the first dim.
+        return tf.nn.embedding_lookup(tf.identity(xs.embs), xs.ids_vec)
+      else:
+        # The above fix tf.tpu_cross_replica_sum causes issues
+        # on inference graphs in which the EmbeddingLayer is on the host
+        # as the tf.identity prevents ResourceGather from being used.
+        return tf.nn.embedding_lookup(xs.embs, xs.ids_vec)
 
     xs = py_utils.NestedMap(embs=embs, ids_vec=ids_vec)
     if self._fprop_mode == 'matmul':
