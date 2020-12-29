Fix off-by-one in matrix power.
PiperOrigin-RevId: 349353990

lingvo/core/matrix_functions.py
@@ -127,7 +127,7 @@ def inlined_matrix_inverse_pth_root(mat_g,
     """
     log2_p = tf.math.log(p) / tf.math.log(tf.constant(2.0, dtype=p.dtype))
     return tf.switch_case(
-        tf.cast(tf.math.round(log2_p), tf.int32), {
+        tf.cast(tf.math.round(log2_p), tf.int32) - 1, {
             0: functools.partial(_unrolled_mat_pow_2, mat_m),
             1: functools.partial(_unrolled_mat_pow_4, mat_m),
             2: functools.partial(_unrolled_mat_pow_8, mat_m),