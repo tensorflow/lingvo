01a21116cc6d37ba5527b6c6acb50b62160ada13
Lingvo Maintenance <lingvo-bot@google.com>
2021-01-13 20:23:35 -0800

    TensorShardingSpec: a util class representing sharding PiperOrigin-RevId: 351720148

--- lingvo/core/gshard_utils.py
+++ lingvo/core/gshard_utils.py
@@ -15,11 +15,14 @@
 # ==============================================================================
 """Utilities for applying xla-sharding to a model."""
 
+from typing import Dict, List, Optional, Sequence
+
 from lingvo import compat as tf
 from lingvo.core import py_utils_flags
 import numpy as np
 
 # pylint: disable=g-direct-tensorflow-import
+from tensorflow.compiler.xla import xla_data_pb2
 from tensorflow.compiler.xla.experimental.xla_sharding import xla_sharding
 # pylint: enable=g-direct-tensorflow-import
 
@@ -127,3 +130,170 @@ def GetNonPod2dMesh(device_mesh_shape, physical_mesh_shape):
   # Next line: zigzag on outer mesh dim (8). It doesn't have wrap link, either.
   device_mesh = ZigzagOrderOnDeviceMesh(device_mesh, zigzag_mesh_dim=0)
   return device_mesh
+
+
+class TensorShardingSpec:
+  """Represents a sharding annotation for GShard/XLA."""
+
+  def __init__(self,
+               split_dims_mapping: Optional[List[int]] = None,
+               device_mesh: Optional[np.ndarray] = None,
+               uneven_padding: Optional[List[int]] = None):
+    """Creates a sharding specification.
+
+    Args:
+      split_dims_mapping: a list of integers that map each tensor axis to the
+        device mesh axis along which it is sharded. Its length is the tensor
+        rank, and split_dims_mapping[i] is device mesh axis for tensor dimension
+        i. Use -1 for tensor dimensions that are not sharded. If the list is set
+        to None, the sharding will be treated as replicated.
+      device_mesh: a numpy.ndarray describing the topology of the device mesh
+        and each element is the ID of the device in the topology. Not needed for
+        replicated sharding, where it can be set to None.
+      uneven_padding: amount of padding applied to the right side of each tensor
+        dimension due to uneven partitioning of the shape in SPMD.
+    """
+    self._split_dims_mapping: Optional[List[int]] = split_dims_mapping
+    self._device_mesh: Optional[np.ndarray] = device_mesh
+    self._uneven_padding = uneven_padding
+
+  @classmethod
+  def FromFullShape(cls, full_shape: Sequence[int],
+                    split_dims_mapping: List[int], device_mesh: np.ndarray):
+    """Creates tiled sharding spec with uneven padding computed from shape."""
+    uneven_padding = [0] * len(split_dims_mapping)
+    for i in range(len(split_dims_mapping)):
+      if split_dims_mapping[i] >= 0:
+        partitions = device_mesh.shape[split_dims_mapping[i]]
+        shard_size = (full_shape[i] + partitions - 1) // partitions
+        uneven_padding[i] = shard_size * partitions - full_shape[i]
+    return TensorShardingSpec(split_dims_mapping, device_mesh, uneven_padding)
+
+  def ApplyToTensor(self,
+                    tensor: tf.Tensor,
+                    use_sharding_op: bool = True) -> tf.Tensor:
+    if self.is_replicated:
+      return xla_sharding.replicate(tensor, use_sharding_op=use_sharding_op)
+    return xla_sharding.mesh_split(
+        tensor,
+        self.device_mesh,
+        self.split_dims_mapping,
+        use_sharding_op=use_sharding_op)
+
+  def ApplyToVariable(self, variable: tf.Variable) -> tf.Variable:
+    if self.is_replicated:
+      return xla_sharding.replicate(variable, use_sharding_op=False)
+    return xla_sharding.mesh_split(
+        variable,
+        self.device_mesh,
+        self.split_dims_mapping,
+        use_sharding_op=False)
+
+  def ShardShape(self, full_shape: Sequence[int]) -> Sequence[int]:
+    """Returns the shape after applying this sharding to full_shape."""
+    if self.is_replicated:
+      return full_shape
+
+    shard_shape = list(full_shape)
+    for i in range(len(self._split_dims_mapping)):
+      if self._split_dims_mapping[i] >= 0:
+        partitions = self._device_mesh.shape[self._split_dims_mapping[i]]
+        shard_shape[i] = (full_shape[i] + partitions - 1) // partitions
+    return shard_shape
+
+  def ManualToAutoPartitioning(self, tensor: tf.Tensor) -> tf.Tensor:
+    """Converts manually sharded tensor to full-size for auto partitioning."""
+    full_shape = list(tensor.shape)
+    if not self.is_replicated:
+      for i in range(len(self._split_dims_mapping)):
+        if self._split_dims_mapping[i] >= 0:
+          full_shape[i] *= self._device_mesh.shape[self._split_dims_mapping[i]]
+        if self._uneven_padding is not None and self._uneven_padding[i] > 0:
+          full_shape[i] -= self._uneven_padding[i]
+    return xla_sharding.manual_to_auto_spmd_partition(
+        tensor,
+        self.ToXlaOpSharding().SerializeToString(), full_shape)
+
+  def AutoToManualPartitioning(self, tensor: tf.Tensor) -> tf.Tensor:
+    """Converts full-size tensor (auto partitioning) to manually sharded."""
+    manual = xla_sharding.auto_to_manual_spmd_partition(
+        tensor,
+        self.ToXlaOpSharding().SerializeToString())
+    xla_sharding.Sharding.manual().apply_to_tensor(manual)
+    return manual
+
+  def ToXlaOpSharding(self) -> xla_data_pb2.OpSharding:
+    if self.is_replicated:
+      return xla_sharding.Sharding.replicate().proto
+    return xla_sharding.mesh_split_sharding(self.device_mesh,
+                                            self.split_dims_mapping).proto
+
+  @classmethod
+  def FromXlaOpSharding(
+      cls, op_sharding_proto: xla_data_pb2.OpSharding) -> 'TensorShardingSpec':
+    """Parses from an XLA OpSharding proto."""
+    if op_sharding_proto.type == xla_data_pb2.OpSharding.OTHER:
+      device_mesh_shape = op_sharding_proto.tile_assignment_dimensions
+      device_mesh = np.reshape(
+          np.array(op_sharding_proto.tile_assignment_devices),
+          device_mesh_shape)
+      if op_sharding_proto.replicate_on_last_tile_dim:
+        split_dims_mapping = list(range(len(device_mesh_shape) - 1))
+      else:
+        split_dims_mapping = list(range(len(device_mesh_shape)))
+      return cls(split_dims_mapping, device_mesh)
+    else:
+      return cls.ReplicatedSpec()
+
+  def AddLeadingDims(self, num_dims: int = 1) -> 'TensorShardingSpec':
+    if self.is_replicated:
+      return self
+    new_padding = (None if self._uneven_padding is None else [0] * num_dims +
+                   self._uneven_padding)
+    return TensorShardingSpec([-1] * num_dims + self._split_dims_mapping,
+                              self.device_mesh, new_padding)
+
+  def RemoveLeadingDims(self, num_dims: int = 1) -> 'TensorShardingSpec':
+    if self.is_replicated:
+      return self
+    new_padding = (None if self._uneven_padding is None else
+                   self._uneven_padding[num_dims:])
+    return TensorShardingSpec(self._split_dims_mapping[num_dims:],
+                              self.device_mesh, new_padding)
+
+  @classmethod
+  def ReplicatedSpec(cls):
+    return TensorShardingSpec()
+
+  @property
+  def split_dims_mapping(self) -> Optional[List[int]]:
+    return self._split_dims_mapping
+
+  @property
+  def device_mesh(self) -> Optional[np.ndarray]:
+    return self._device_mesh
+
+  @property
+  def is_replicated(self) -> bool:
+    return self.split_dims_mapping is None or max(self.split_dims_mapping) < 0
+
+  @property
+  def mesh_dim_to_tensor_dim_mapping(self) -> Dict[int, int]:
+    mapping = {}
+    if self.is_replicated:
+      return mapping
+    for i in range(len(self.split_dims_mapping)):
+      if self.split_dims_mapping[i] >= 0:
+        mapping[self.split_dims_mapping[i]] = i
+    return mapping
+
+
+def GetVarSharding(var: tf.Variable) -> TensorShardingSpec:
+  """Returns the sharding directly attached to a variable."""
+  sharding = xla_sharding.get_op_sharding(var.op)
+  if not sharding:
+    return TensorShardingSpec.ReplicatedSpec()
+
+  proto = xla_data_pb2.OpSharding()
+  proto.ParseFromString(sharding)
+  return TensorShardingSpec.FromXlaOpSharding(proto)
