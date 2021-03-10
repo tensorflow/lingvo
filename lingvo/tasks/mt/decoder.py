Fix decoder.single_token_fast_decode: Use a smaller value for a sufficiently large number to avoid numerical issues.
PiperOrigin-RevId: 361971115

lingvo/tasks/mt/decoder.py
@@ -396,6 +396,11 @@ class MTBaseDecoder(base_decoder.BaseBeamSearchDecoder):
 class MTDecoderV1(MTBaseDecoder, quant_utils.QuantizableLayer):
   """MT decoder v1."""
 
+  # We scale a float's dtype.max by this amount to stand in for a
+  # sufficiently large number. This is chosen such that for log_prob values,
+  # accumulating it over beam search steps will not cause numerical issues.
+  _FLOAT_DTYPE_MAX_SCALER = 0.00001
+
   @classmethod
   def Params(cls):
     p = super().Params()
@@ -442,8 +447,6 @@ class MTDecoderV1(MTBaseDecoder, quant_utils.QuantizableLayer):
     p.Define(
         'sentence_boundary_token_id', None,
         'None, or an int. The token id that separates different sentences.')
-    # TODO(b/181916643): ensure this works with TPU beam search, which it does
-    # not currently for unknown reasons.
     p.Define(
         'single_token_fast_decode', False,
         'bool. When enabled, for input of length 1, decoding completes in 1 '
@@ -1026,7 +1029,8 @@ class MTDecoderV1(MTBaseDecoder, quant_utils.QuantizableLayer):
     # the current hyp contains fewer sentences than expected to disallow
     # eos in such misaligned cases.
     large_negative_value = tf.ones_like(log_probs[:, eos_id]) * tf.constant(
-        -0.1, dtype=log_probs.dtype) * log_probs.dtype.max
+        -self._FLOAT_DTYPE_MAX_SCALER,
+        dtype=log_probs.dtype) * log_probs.dtype.max
     eos_log_probs = tf.where(
         tf.math.greater(source_num_sentences, hyp_num_sentences),
         large_negative_value, log_probs[:, eos_id])
@@ -1069,7 +1073,8 @@ class MTDecoderV1(MTBaseDecoder, quant_utils.QuantizableLayer):
     is_eos = tf.math.equal(tf.range(v), tf.ones_like(tf.range(v)) * eos_id)
     is_eos = tf.tile(tf.expand_dims(is_eos, 0), [b, 1])
     large_neg_probs = tf.ones_like(log_probs) * tf.constant(
-        -0.1, dtype=log_probs.dtype) * log_probs.dtype.max
+        -self._FLOAT_DTYPE_MAX_SCALER,
+        dtype=log_probs.dtype) * log_probs.dtype.max
     new_log_probs = tf.where(is_eos, tf.zeros_like(large_neg_probs),
                              large_neg_probs)
     return tf.where(is_single_token_2d, new_log_probs, log_probs)