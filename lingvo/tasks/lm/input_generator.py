30afc0740a609eb00f9ee6e54098cb6651e14c33
Lingvo Maintenance <lingvo-bot@google.com>
2021-09-28 22:37:56 -0700

    Move some code to lingvo.
    PiperOrigin-RevId: 399607705

--- lingvo/tasks/lm/input_generator.py
+++ lingvo/tasks/lm/input_generator.py
@@ -18,8 +18,11 @@
 import lingvo.compat as tf
 from lingvo.core import base_input_generator
 from lingvo.core import generic_input
+from lingvo.core import ops
 from lingvo.core import py_utils
+from lingvo.core import summary_utils
 from lingvo.core import tokenizers
+from lingvo.tasks.lm import tokenizer as lm_tokenizer
 
 
 class LmInput(base_input_generator.BaseSequenceInputGenerator):
@@ -96,3 +99,264 @@ class LmInput(base_input_generator.BaseSequenceInputGenerator):
       SetShape(ret.weights)
       ret.word_count.set_shape([bs])
     return ret
+
+
+class PackedTextInputGenerator(base_input_generator.BaseSequenceInputGenerator):
+  """Input generator reading tf.Examples of texts with packing."""
+
+  @classmethod
+  def Params(cls):
+    p = super().Params()
+    p.Define('enable_packing', False,
+             'Whether to pack multiple documents on the same row.')
+    p.Define('shuffle', True, 'Whether to randomly shuffle.')
+    p.Define(
+        'prepacking_batch_size', 1 << 14,
+        'Only used when p.enable_packing is set. The batch size before we pack. '
+        'Note that this does not affect post-packing batch size but may '
+        'have a minor effect on how tight the packed output is.')
+    p.Define(
+        'dataset_type', None,
+        'A dataset class constructor such as tf.data.TFRecordDataset. '
+        'The class constructor must take a list of filenames and produce an '
+        'object that extends tf.data.Dataset.')
+    p.tokenizer = lm_tokenizer.BertTokenizer.Params()
+    p.source_max_length = 512
+    p.pad_to_max_seq_length = True
+    return p
+
+  def __init__(self, params):
+    super().__init__(params)
+    p = self.params
+    assert p.source_max_length
+    if self.do_eval:
+      p.shuffle = False
+    if isinstance(p.file_pattern, str):
+      p.file_pattern = [p.file_pattern]
+
+  def _ProcessSingleExample(self, key, record):
+    """Parses a serialized tf.Example record."""
+    p = self.params
+    del key
+    features = tf.io.parse_single_example(
+        serialized=record,
+        features={
+            'text':
+                tf.io.FixedLenFeature((), dtype=tf.string, default_value=''),
+        })
+    ids, labels, paddings = self.StringsToIds(
+        [features['text']], external_max_length=p.source_max_length)
+    ret = py_utils.NestedMap(ids=ids, labels=labels, paddings=paddings)
+    ret.weights = 1.0 - ret.paddings
+    return ret
+
+  def _DatasetOfExamples(self):
+    """Returns tf.data.Dataset of tf.Examples from p.file_pattern."""
+    p = self.params
+    file_patterns = p.file_pattern
+    weights = None
+    if all([isinstance(x, tuple) for x in p.file_pattern]):
+      if self.do_eval:
+        raise ValueError('Sampling with weights not support for eval data.')
+      file_patterns, weights = zip(*p.file_pattern)
+
+    file_patterns = list(map(py_utils.ShardedFilePatternToGlob, file_patterns))
+    tf.logging.info(f'Mixing files {file_patterns} with weights {weights}.')
+
+    def _Load(file_pattern):
+      dataset = tf.data.Dataset.list_files(
+          file_pattern, shuffle=p.shuffle).interleave(
+              p.dataset_type,
+              cycle_length=tf.data.AUTOTUNE if p.shuffle else 1,
+              num_parallel_calls=tf.data.AUTOTUNE)
+      if p.shuffle:
+        dataset = dataset.shuffle(p.file_buffer_size)
+      dataset = dataset.repeat(1 if self.do_eval else -1)
+      return dataset
+
+    if weights is None or len(weights) <= 1:
+      return _Load(file_patterns)
+
+    return tf.data.experimental.sample_from_datasets(
+        [_Load(f) for f in file_patterns], weights)
+
+  def _GetPaddingValues(self, add_segment_fields=False):
+    ret = py_utils.NestedMap(ids=0, labels=0, weights=0.0, paddings=1.0)
+    if add_segment_fields:
+      ret.segment_ids = 0.0
+      ret.segment_pos = 0
+    return ret
+
+  def _InputBatch(self):
+    """Returns tf.data.Dataset of unbatched NestedMap."""
+    p = self.params
+    dataset = self._DatasetOfExamples()
+    dataset = dataset.map(
+        self._ProcessSingleExample,
+        num_parallel_calls=tf.data.AUTOTUNE).unbatch()
+    dataset = dataset.take(p.num_samples if p.num_samples > 0 else -1)
+
+    if p.enable_packing:
+      dataset = dataset.batch(
+          p.prepacking_batch_size,
+          drop_remainder=True,
+          num_parallel_calls=tf.data.AUTOTUNE if p.shuffle else 1).map(
+              self._Pack, num_parallel_calls=tf.data.AUTOTUNE).unbatch()
+
+    dataset = dataset.batch(
+        self.InfeedBatchSize(),
+        drop_remainder=True,
+        num_parallel_calls=tf.data.AUTOTUNE if p.shuffle else 1,
+    )
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    iterator = tf.data.make_initializable_iterator(dataset)
+    self._initializer = iterator.initializer
+    batch = iterator.get_next()
+
+    if not hasattr(batch, 'segment_ids'):
+      # Supply segment_ids and segment_pos with no packing.
+      batch.segment_ids = 1.0 - batch.paddings
+      segpos = tf.cast(tf.range(p.source_max_length), dtype=tf.float32)
+      batch.segment_pos = tf.cast(batch.segment_ids * segpos, dtype=tf.int32)
+
+    def ShapeAndCast(x):
+      x = tf.ensure_shape(x, (self.InfeedBatchSize(), p.source_max_length))
+      if x.dtype.is_floating:
+        x = tf.cast(x, py_utils.FPropDtype(p))
+      return x
+
+    batch = batch.Transform(ShapeAndCast)
+
+    num_samples = tf.math.reduce_max(batch.segment_ids, axis=1)
+    summary_utils.scalar('examples/num_packed_samples',
+                         tf.reduce_sum(num_samples))
+    return batch
+
+  def _Pack(self, batch_in):
+    """Packs a given batch, which changes the batch size."""
+    assert not hasattr(batch_in, 'segment_ids')
+    assert not hasattr(batch_in, 'segment_pos')
+
+    actual_seq_len = tf.math.reduce_sum(
+        tf.cast(batch_in.weights, tf.int32), axis=1)
+    (segment_ids, segment_pos, indices_in_input, _, _, _) = ops.pack_sequences(
+        actual_seq_len,
+        actual_seq_len,
+        packed_batch_size=0,
+        packed_src_seq_len=self.params.source_max_length,
+        packed_tgt_seq_len=self.params.source_max_length)
+
+    def ApplyPacking(x):
+      return ops.apply_packing(x, 0, segment_ids, indices_in_input)
+
+    batch_out = batch_in.DeepCopy()
+    batch_out = batch_out.Transform(ApplyPacking)
+    batch_out.paddings = ops.apply_packing(batch_in.paddings, 1, segment_ids,
+                                           indices_in_input)
+    batch_out.segment_ids = tf.cast(segment_ids, tf.float32)
+    batch_out.segment_pos = segment_pos
+
+    return batch_out
+
+  def Reset(self, tf_session):
+    tf_session.run(self._initializer)
+
+  def Initialize(self, sess):
+    sess.run(self._initializer)
+
+
+class TFRecordBertInput(base_input_generator.BaseInputGenerator):
+  """Input generator reading TFRecords of ids for MLPerf eval."""
+
+  @classmethod
+  def Params(cls):
+    p = super().Params()
+    # https://github.com/mlcommons/training/tree/master/language_model/tensorflow/bert#tfrecord-features
+    p.Define('input_file', None, 'String, path of an input file.')
+    p.Define('max_sequence_length', 512,
+             'Maximum number of tokens to be present in a single example.')
+    p.Define('max_predictions_per_seq', 76,
+             'Maximum number of tokens that can be masked per example.')
+    return p
+
+  def __init__(self, params):
+    super().__init__(params)
+    p = self.params
+    p.resettable = True
+
+  def _ParseRecord(self, record):
+    """Reads and parses a single record."""
+    p = self.params
+    name_to_features = {
+        'input_ids':
+            tf.io.FixedLenFeature([p.max_sequence_length], tf.int64),
+        'input_mask':
+            tf.io.FixedLenFeature([p.max_sequence_length], tf.int64),
+        'masked_lm_positions':
+            tf.io.FixedLenFeature([p.max_predictions_per_seq], tf.int64),
+        'masked_lm_ids':
+            tf.io.FixedLenFeature([p.max_predictions_per_seq], tf.int64),
+        'masked_lm_weights':
+            tf.io.FixedLenFeature([p.max_predictions_per_seq], tf.float32),
+    }
+    example = tf.io.parse_single_example(record, name_to_features)
+    mask_length = tf.cast(
+        tf.reduce_sum(example['masked_lm_weights']), dtype=tf.int32)
+    masked_lm_positions = tf.slice(example['masked_lm_positions'], [0],
+                                   [mask_length])
+    masked_lm_ids = tf.cast(
+        tf.slice(example['masked_lm_ids'], [0], [mask_length]), dtype=tf.int32)
+    ret = py_utils.NestedMap()
+    ret.masked_ids = tf.cast(example['input_ids'], dtype=tf.int32)
+    # Get back non-masked, original ids.
+    ret.ids = tf.tensor_scatter_nd_update(
+        tensor=ret.masked_ids,
+        indices=tf.reshape(masked_lm_positions, [-1, 1]),
+        updates=masked_lm_ids)
+    ret.masked_pos = tf.tensor_scatter_nd_update(
+        tensor=tf.zeros_like(ret.masked_ids, dtype=tf.float32),
+        indices=tf.reshape(masked_lm_positions, [-1, 1]),
+        updates=tf.ones_like(masked_lm_ids, dtype=tf.float32))
+    ret.segment_ids = tf.cast(example['input_mask'], dtype=tf.float32)
+    ret.paddings = tf.cast(1 - example['input_mask'], dtype=tf.float32)
+    pos = tf.cast(tf.range(p.max_sequence_length), dtype=tf.float32)
+    ret.segment_pos = tf.cast(ret.segment_ids * pos, dtype=tf.int32)
+    return ret
+
+  def _PadToBatchSize(self, batch):
+
+    def Pad(key, t):
+      constant_v = 0
+      if t.dtype.is_floating and key.endswith('.paddings'):
+        constant_v = 1.0
+      need = self.params.batch_size - py_utils.GetShape(t)[0]
+      padded = tf.pad(t, [[0, need], [0, 0]], 'CONSTANT', constant_v)
+      return padded
+
+    return batch.TransformWithKey(Pad)
+
+  def _InputBatch(self):
+    """Returns a batch with .ids, .masked_ids, and .masked_pos."""
+    p = self.params
+    dataset = tf.data.TFRecordDataset([p.input_file]).map(self._ParseRecord)
+    dataset = dataset.batch(batch_size=p.batch_size, drop_remainder=False)
+    dataset = dataset.map(self._PadToBatchSize)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    iterator = tf.data.make_initializable_iterator(dataset)
+    self._initializer = iterator.initializer
+    batch = iterator.get_next()
+
+    def Cast(x):
+      x = tf.ensure_shape(x, [p.batch_size, p.max_sequence_length])
+      if x.dtype.is_floating:
+        x = tf.cast(x, py_utils.FPropDtype(p))
+      return x
+
+    return batch.Transform(Cast)
+
+  def Reset(self, tf_session):
+    tf_session.run(self._initializer)
+
+  def Initialize(self, sess):
+    sess.run(self._initializer)
---
+++ lingvo/tasks/lm/tokenizer.py
@@ -0,0 +1,126 @@
+# Lint as: python3
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tokenizers."""
+
+from lingvo import compat as tf
+from lingvo.core import py_utils
+from lingvo.core import tokenizers
+
+import tensorflow_text as tf_text
+
+
+class BertTokenizer(tokenizers.BaseTokenizer):
+  """A WordPiece tokenizer used by BERT."""
+
+  @classmethod
+  def Params(cls):
+    p = super().Params()
+    p.Define('vocab_path', None, 'Path to the vocab file.')
+    p.Define(
+        'prepend_sos', False,
+        'Whether to always prepend SOS token. This makes ids and target '
+        'returned identical and effective length longer by 1.')
+    p.target_unk_id = 100
+    p.target_sos_id = 101
+    p.target_eos_id = 102
+    return p
+
+  def __init__(self, params):
+    super().__init__(params)
+    self._tokenizer = tf_text.BertTokenizer(
+        params.vocab_path,
+        lower_case=True,
+        max_bytes_per_word=200,
+        token_out_type=tf.int32,
+    )
+
+  def _StringsToIdsImpl(self, strs, max_length, append_eos, languages):
+    del languages
+    p = self.params
+    if append_eos is None:
+      append_eos = p.append_eos
+
+    batch_size = py_utils.GetShape(strs)[0]
+    token_ids_ta = tf.TensorArray(tf.int32, batch_size)
+    target_ids_ta = tf.TensorArray(tf.int32, batch_size)
+    paddings_ta = tf.TensorArray(tf.float32, batch_size)
+
+    def _TokenizeOneSentence(i, text, token_ids_ta, target_ids_ta, paddings_ta):
+      """Tokenizes a single sentence."""
+      if tf.is_tensor(i):
+        text_i = tf.gather(text, i)
+      else:
+        text_i = text[i]
+      ids = self._tokenizer.tokenize(text_i).merge_dims(0, -1)
+      ids.set_shape([None])
+
+      if append_eos:
+        ids = tf.concat([ids, [self.eos_id]], axis=0)
+      sos_ids = tf.concat([[self.sos_id], ids], axis=0)
+      if p.prepend_sos:
+        ids = sos_ids
+
+      # This truncates after the EOS is added, so some sentences might
+      # not have EOS at the end.
+      token_ids_ta = token_ids_ta.write(
+          i, py_utils.PadOrTrimTo(sos_ids, [max_length], 0))
+      target_ids_ta = target_ids_ta.write(
+          i, py_utils.PadOrTrimTo(ids, [max_length], 0))
+      paddings_ta = paddings_ta.write(
+          i,
+          py_utils.PadOrTrimTo(
+              tf.zeros_like(ids, dtype=tf.float32), [max_length], 1.))
+
+      return i + 1, strs, token_ids_ta, target_ids_ta, paddings_ta
+
+    _, _, token_ids_ta, target_ids_ta, paddings_ta = tf.while_loop(
+        lambda i, *_: i < batch_size,
+        _TokenizeOneSentence,
+        loop_vars=(tf.constant(0, tf.int32), strs, token_ids_ta, target_ids_ta,
+                   paddings_ta),
+        parallel_iterations=30,
+        back_prop=False)
+
+    token_ids = token_ids_ta.stack()
+    target_ids = target_ids_ta.stack()
+    paddings = paddings_ta.stack()
+
+    if not p.pad_to_max_length:
+      maxlen = tf.cast(
+          tf.round(tf.reduce_max(tf.reduce_sum(1.0 - paddings, axis=1))),
+          tf.int32)
+      token_ids = token_ids[:, :maxlen]
+      target_ids = target_ids[:, :maxlen]
+      paddings = paddings[:, :maxlen]
+
+    return token_ids, target_ids, paddings
+
+  def IdsToStrings(self, ids, lens):
+    """Takes int32 token ids and returns approximate detokenized strings."""
+    ids = py_utils.with_dependencies([py_utils.assert_same_dim0([ids, lens])],
+                                     ids)
+
+    def _ProcessRow(inputs):
+      length = inputs[1]
+      ids = tf.reshape(inputs[0][:length], [1, -1])
+      tokens = self._tokenizer.detokenize(ids)
+      return tf.strings.reduce_join(tokens.flat_values, separator=' ')
+
+    return tf.map_fn(
+        _ProcessRow, (ids, lens),
+        dtype=tf.string,
+        parallel_iterations=30,
+        back_prop=False)
