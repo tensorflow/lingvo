Fix adjust gradients helper function in eager mode.
PiperOrigin-RevId: 412978983

lingvo/core/py_utils.py
@@ -3189,7 +3189,8 @@ def AdjustGradientsWithLpLoss(var_grads, lp_regularizer_weight, p=2.0):
   ]
   filtered_vars = Transform(GetVar, filtered_var_grads)
   for v in filtered_vars:
-    tf.logging.info('AdjustGradientsWithLpLoss: %s', v.name)
+    v_name = v.name if not tf.executing_eagerly() else '[eager]'
+    tf.logging.info('AdjustGradientsWithLpLoss: %s', v_name)
 
   if p == 2.0:
     lp_loss = 0.5 * lp_regularizer_weight * SumSquared(filtered_vars)