dac0f04cb5692829f08afdb0872742725ae82d26
Wei Han <weihan@google.com>
2022-03-02 17:22:52 -0800

    Adds a helper function to serialize NestedMap as lingvo.cop.ops.Record.
    PiperOrigin-RevId: 432067213

--- lingvo/core/decoder_lib.py
+++ lingvo/core/decoder_lib.py
@@ -17,8 +17,29 @@
 
 import pickle
 
+import lingvo.compat as tf
+from lingvo.core import py_utils
+from lingvo.core.ops import record_pb2
+
 
 def WriteKeyValuePairs(filename, key_value_pairs):
   """Writes `key_value_pairs` to `filename`."""
   with open(filename, 'wb') as f:
     pickle.dump(key_value_pairs, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def SerializeOutputs(nmap: py_utils.NestedMap) -> bytes:
+  """Return a serialized representation of the contents of `nmap`.
+
+  Args:
+    nmap: A NestedMap of data to serialize.
+
+  Returns:
+    A serialized record_pb2.Record() of the contents of `nmap`.
+  """
+  record = record_pb2.Record()
+  flat_nmap = nmap.FlattenItems()
+  for key, value in flat_nmap:
+    record.fields[key].CopyFrom(tf.make_tensor_proto(value))
+  serialized = record.SerializeToString()
+  return serialized
